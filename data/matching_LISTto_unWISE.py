3829a79b56b353158887f5b674437a766ae90eca
npross <npross@lbl.gov>
2018-11-07 13:17:15 -0800

    clean-up, update of data and color_color

--- data/matching_LISTto_unWISE.py
+++ data/matching_LISTto_unWISE.py
@@ -30,8 +30,8 @@ counter_justW1_match  = 0
 counter_justW2_match  = 0
 counter_nomatch       = 0
 
-
-for ii,jj in zip(VHzQs['ra'], VHzQs['dec']) :
+print('abcd count        RA           Dec   redshift  unW1_RA    unW1_Dec     unW2_RA    unW2_Dec     unW1_mag unW1_magerr    unW1_snr     unW2_mag unW2_magerr    unW2_snr idx_w1 idx_w2    sep_w1         sep_w2', file=f)
+for ii,jj,kk in zip(VHzQs['ra'], VHzQs['dec'], VHzQs['redshift'])  :
     counter = counter + 1
 
     delta_ra_W1  = ii - unWISE_W1['ra']
@@ -47,12 +47,13 @@ for ii,jj in zip(VHzQs['ra'], VHzQs['dec']) :
     idx_w2       = np.argmin(delta_w2)
     am_w2, bm_w2 = VHzQs[idx_w2], unWISE_W2[idx_w2]
 
+
     ## Now go into the logic
     ## First both W1 AND W2 mathced..
     if (  ((delta_w1.min()*3600.) < dist) and ((delta_w2.min()*3600.) < dist) ):
         counter_both_matched = counter_both_matched + 1 
-        print('A,{: 4d}{: 14.8f}{: 14.8f} {: 12.5f}{: 12.5f}{: 12.5f}{: 12.5f} {: 12.5f}{: 12.5f}{: 12.5f} {: 12.5f}{: 12.5f}{: 12.5f} {: 4d}{: 4d} {: 14.6f} {: 14.6f} '.format(
-            counter, ii, jj,
+        print('A,{: 4d}{: 14.8f}{: 14.8f} {:6.2f} {: 12.5f}{: 12.5f}{: 12.5f}{: 12.5f} {: 12.5f}{: 12.5f}{: 12.5f} {: 12.5f}{: 12.5f}{: 12.5f} {: 4d}{: 4d} {: 14.6f} {: 14.6f} '.format(
+            counter, ii, jj, kk, 
             bm_w1['ra'],     bm_w1['dec'],       bm_w2['ra'],    bm_w2['dec'],
             bm_w1['w1mpro'], bm_w1['w1sigmpro'], bm_w1['w1snr'],
             bm_w2['w2mpro'], bm_w2['w2sigmpro'], bm_w2['w2snr'], 
@@ -61,8 +62,8 @@ for ii,jj in zip(VHzQs['ra'], VHzQs['dec']) :
     ## only W1 mathced..
     elif (  ((delta_w1.min()*3600.) < dist) and ((delta_w2.min()*3600.) > dist) ):
         counter_justW1_match = counter_justW1_match + 1
-        print('B,{: 4d}{: 14.8f}{: 14.8f} {: 12.5f}{: 12.5f}{: 12.5f}{: 12.5f} {: 12.5f}{: 12.5f}{: 12.5f}  {:11.5f}{:12.5f}{:12.5f} {: 4d}{: 4d} {: 14.6f} {: 14.6f} '.format(
-            counter, ii, jj,
+        print('B,{: 4d}{: 14.8f}{: 14.8f} {:6.2f} {: 12.5f}{: 12.5f}{: 12.5f}{: 12.5f} {: 12.5f}{: 12.5f}{: 12.5f}  {:11.5f}{:12.5f}{:12.5f} {: 4d}{: 4d} {: 14.6f} {: 14.6f} '.format(
+            counter, ii, jj, kk, 
             bm_w1['ra'],     bm_w1['dec'],       -99.999, -99.999,
             bm_w1['w1mpro'], bm_w1['w1sigmpro'], bm_w1['w1snr'],
             -99.999, -9.99, -99.999, 
@@ -71,16 +72,16 @@ for ii,jj in zip(VHzQs['ra'], VHzQs['dec']) :
     ## only W2 mathced..
     elif (  ((delta_w1.min()*3600.) > dist) and ((delta_w2.min()*3600.) < dist) ):
         counter_justW2_match = counter_justW2_match + 1
-        print('C,{: 4d}{: 14.8f}{: 14.8f} {: 12.5f}{: 12.5f}{: 12.5f}{: 12.5f}  {: 11.5f}{: 12.5f}{: 12.5f} {: 12.5f}{: 12.5f}{: 12.5f} {: 4d}{: 4d} {: 14.6f}{: 14.6f} '.format(
-            counter, ii, jj,
+        print('C,{: 4d}{: 14.8f}{: 14.8f} {:6.2f} {: 12.5f}{: 12.5f}{: 12.5f}{: 12.5f}  {: 11.5f}{: 12.5f}{: 12.5f} {: 12.5f}{: 12.5f}{: 12.5f} {: 4d}{: 4d} {: 14.6f}{: 14.6f} '.format(
+            counter, ii, jj, kk, 
             -99.999, -99.999, bm_w2['ra'], bm_w2['dec'],
             -99.999, -9.99, -99.999, 
             bm_w2['w2mpro'], bm_w2['w2sigmpro'], bm_w2['w2snr'], 
             idx_w1, idx_w2, delta_w1.min()*3600., delta_w2.min()*3600.,),  file=f)             
               
     else:
-        print('D,{: 4d}{: 14.8f}{: 14.8f} {: 12.5f}{: 12.5f}{: 12.5f}{: 12.5f} {: 12.5f}{: 12.5f}{: 12.5f}  {: 11.5f}{:12.5f}{:12.5f} {: 4d}{: 4d} {: 14.6f} {: 14.6f} '.format(
-            counter, ii, jj,
+        print('D,{: 4d}{: 14.8f}{: 14.8f} {:6.2f} {: 12.5f}{: 12.5f}{: 12.5f}{: 12.5f} {: 12.5f}{: 12.5f}{: 12.5f}  {: 11.5f}{:12.5f}{:12.5f} {: 4d}{: 4d} {: 14.6f} {: 14.6f} '.format(
+            counter, ii, jj, kk, 
             -99.999, -99.999, -99.999, -99.999,
             -99.999, -9.99, -99.999,
             -99.999, -9.99, -99.999,
@@ -95,6 +96,6 @@ print('Matched in W1   :: ',  counter_both_matched+counter_justW1_match)
 print('Matched in W2   :: ',  counter_both_matched+counter_justW2_match)
 print('Neither matched :: ',  counter_nomatch)
 print()
-print('Total mathched = ', counter_both_matched+counter_justW1_match+counter_justW2_match+counter_nomatch, 'vs. total counters', counter)
+print('Total matched   = ', counter_both_matched+counter_justW1_match+counter_justW2_match+counter_nomatch, 'vs. total counters', counter)
 f.close() 
 
