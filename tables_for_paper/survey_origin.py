ecf95ca493b31f6d75c076c313e573ee6919f21e
npross <npross@lbl.gov>
2018-12-08 10:41:40 +0000

    WW4C_VHzQ .tex, .pdf udpated

--- tables_for_paper/survey_origin.py
+++ tables_for_paper/survey_origin.py
@@ -1,14 +1,9 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
-''' A simple little bit of code to figure out which survey the VHzQs came from.'''
-
-
-# In[2]:
-
+'''
+A simple little bit of code to figure out which survey the VHzQs came
+from.  I haven't gone "fully, fully" here's the plug in .tex, but
+could easily see that happening and would be happy for it!!
+v1.00    Wed Dec  5 19:14:34 GMT 2018
+'''
 
 import numpy as np
 import pandas as pd
@@ -17,61 +12,33 @@ from astropy.io    import fits
 from astropy.io    import ascii
 from astropy.table import Table
 
-
-# In[3]:
-
-
+##
 ##  V H z Q    data
+##
 path     = '../data/'
 filename = 'LIST_OF_VHzQs.dat'
 table    = path+filename
 
 VHzQ_list = ascii.read(table)
 
-
-# In[4]:
-
-
+## Just a little bit of info 
 print('type(VHzQ_list)', type(VHzQ_list))
 print('len(VHzQ_list)',   len(VHzQ_list))
-
-
-# In[5]:
-
-
 VHzQ_list.colnames
-
-
-# In[6]:
-
 
 ## Column 'na' has the original survey designations
 len(np.unique(VHzQ_list['na']))
-
-
-# In[7]:
-
-
+#type(np.unique(VHzQ_list['na']))
 np.unique(VHzQ_list['na'])
 
-
-# In[8]:
-
-
-type(np.unique(VHzQ_list['na']))
-
-
-# In[9]:
-
-
-#VHzQ_list.group_by('na').size().sort_values(ascending=False)
+## Nice, cute way to pick out the survey and the number of
+## objects from that survey -- thanks @nudomarinero !!
 survey, counts = np.unique(VHzQ_list["na"], return_counts=True)
 
-
-# In[11]:
-
-
 ## Loop through the number of entries the variable 'survey' has
+print()
 for x in range(len(survey)):
     print('{:7} & {:4d}'.format(survey[x], counts[x]))
+print()
+    
 
