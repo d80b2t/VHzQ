4277ff0f54b36b19aaf31d5e9d0d54bbdc789ea7
npross <npross@lbl.gov>
2019-02-11 09:36:18 +0000

    big commit of all the weekends work

--- light_curves/lightcurve_plot.py
+++ light_curves/lightcurve_plot.py
@@ -14,7 +14,7 @@ from astropy.io import ascii
 path = '/cos_pc19a_npr/programs/quasars/highest_z/data/LightCurves/'
 
 
-choice = '4'
+choice = '1'
 ## Ordered by qsoID number. 
 print()
 print('  1)   MMTJ0215-0529      (qsoID  54)')
@@ -39,43 +39,60 @@ print
 ##  P h o t o m e t r y    d a t a
 ##
 
+## all: all the photometric data
+## 30d: photometry averaged over a 30 MJD period
+##	
+
 ##  WSA  - WFCAM Science Archive
-WSA_data = ascii.read(path+inputQuasar+'/WSA_lc.dat')
+WSA_data_all = ascii.read(path+inputQuasar+'/WSA_lc_all.dat')
+WSA_data_30d = ascii.read(path+inputQuasar+'/WSA_lc_30d.dat')
 
 ## VSA - VISTA Science Archive
-VSA_data = ascii.read(path+inputQuasar+'/VSA_lc.dat')
+VSA_data_all = ascii.read(path+inputQuasar+'/VSA_lc_all.dat')
+VSA_data_30d = ascii.read(path+inputQuasar+'/VSA_lc_30d.dat')
+
+
+##  Assigned in WSA: 1=Z,2=Y,3=J,4=H,5=K,6=H2,7=Br,8=blank)
+Z_WFCAM_all = WSA_data_all[(WSA_data_all['filterID'] == 1) & (WSA_data_all['aperMag3AverAB']>0)]
+Y_WFCAM_all = WSA_data_all[(WSA_data_all['filterID'] == 2) & (WSA_data_all['aperMag3AverAB']>0)]
+J_WFCAM_all = WSA_data_all[(WSA_data_all['filterID'] == 3) & (WSA_data_all['aperMag3AverAB']>0)]
+H_WFCAM_all = WSA_data_all[(WSA_data_all['filterID'] == 4) & (WSA_data_all['aperMag3AverAB']>0)]
+K_WFCAM_all = WSA_data_all[(WSA_data_all['filterID'] == 5) & (WSA_data_all['aperMag3AverAB']>0)]
+
+Z_WFCAM_30d = WSA_data_30d[(WSA_data_30d['filterID'] == 1) & (WSA_data_30d['aperMag3Ab']>0)]
+Y_WFCAM_30d = WSA_data_30d[(WSA_data_30d['filterID'] == 2) & (WSA_data_30d['aperMag3Ab']>0)]
+J_WFCAM_30d = WSA_data_30d[(WSA_data_30d['filterID'] == 3) & (WSA_data_30d['aperMag3Ab']>0)]
+H_WFCAM_30d = WSA_data_30d[(WSA_data_30d['filterID'] == 4) & (WSA_data_30d['aperMag3Ab']>0)]
+K_WFCAM_30d = WSA_data_30d[(WSA_data_30d['filterID'] == 5) & (WSA_data_30d['aperMag3Ab']>0)]
+
+##  W F C A M      (from the Ross & Cross appendix) 
+#Z_ABw_all = Z_WFCAM_all['aperMag3AverAB'] + 0.514
+#Y_ABw_all = Y_WFCAM_all['aperMag3AverAB'] + 0.617
+#J_ABw_all = J_WFCAM_all['aperMag3AverAB'] + 0.919 
+#H_ABw_all = H_WFCAM_all['aperMag3AverAB'] + 1.379
+#K_AB_all  = K_WFCAM_all['aperMag3AverAB'] + 1.900
 
 
 ##  UID of combined filter (assigned in WSA: 1=Z,2=Y,3=J,4=H,5=K,6=H2,7=Br,8=blank)
-Z_WFCAM = WSA_data[(WSA_data['filterID'] == 1) & (WSA_data['aperMag3AverAB']>0)]
-Y_WFCAM = WSA_data[(WSA_data['filterID'] == 2) & (WSA_data['aperMag3AverAB']>0)]
-J_WFCAM = WSA_data[(WSA_data['filterID'] == 3) & (WSA_data['aperMag3AverAB']>0)]
-H_WFCAM = WSA_data[(WSA_data['filterID'] == 4) & (WSA_data['aperMag3AverAB']>0)]
-K_WFCAM = WSA_data[(WSA_data['filterID'] == 5) & (WSA_data['aperMag3AverAB']>0)]
+Z_VIRCAM_all  = VSA_data_all[(VSA_data_all['filterID'] == 1) & (VSA_data_all['aperMag3AverAB']>0)]
+Y_VIRCAM_all  = VSA_data_all[(VSA_data_all['filterID'] == 2) & (VSA_data_all['aperMag3AverAB']>0)]
+J_VIRCAM_all  = VSA_data_all[(VSA_data_all['filterID'] == 3) & (VSA_data_all['aperMag3AverAB']>0)]
+H_VIRCAM_all  = VSA_data_all[(VSA_data_all['filterID'] == 4) & (VSA_data_all['aperMag3AverAB']>0)]
+Ks_VIRCAM_all = VSA_data_all[(VSA_data_all['filterID'] == 5) & (VSA_data_all['aperMag3AverAB']>0)]
+
+Z_VIRCAM_30d  = VSA_data_30d[(VSA_data_30d['filterID'] == 1) & (VSA_data_30d['aperMag3Ab']>0)]
+Y_VIRCAM_30d  = VSA_data_30d[(VSA_data_30d['filterID'] == 2) & (VSA_data_30d['aperMag3Ab']>0)]
+J_VIRCAM_30d  = VSA_data_30d[(VSA_data_30d['filterID'] == 3) & (VSA_data_30d['aperMag3Ab']>0)]
+H_VIRCAM_30d  = VSA_data_30d[(VSA_data_30d['filterID'] == 4) & (VSA_data_30d['aperMag3Ab']>0)]
+Ks_VIRCAM_30d = VSA_data_30d[(VSA_data_30d['filterID'] == 5) & (VSA_data_30d['aperMag3Ab']>0)]
 
 
-##  UID of combined filter (assigned in WSA: 1=Z,2=Y,3=J,4=H,5=K,6=H2,7=Br,8=blank)
-Z_VIRCAM  = VSA_data[(VSA_data['filterID'] == 1) & (VSA_data['aperMag3AverAB']>0)]
-Y_VIRCAM  = VSA_data[(VSA_data['filterID'] == 2) & (VSA_data['aperMag3AverAB']>0)]
-J_VIRCAM  = VSA_data[(VSA_data['filterID'] == 3) & (VSA_data['aperMag3AverAB']>0)]
-H_VIRCAM  = VSA_data[(VSA_data['filterID'] == 4) & (VSA_data['aperMag3AverAB']>0)]
-Ks_VIRCAM = VSA_data[(VSA_data['filterID'] == 5) & (VSA_data['aperMag3AverAB']>0)]
-
-
-##  From the Ross&Cross appendix
-##  WFCAM
-Z_ABw = Z_WFCAM['aperMag3AverAB'] + 0.514
-Y_ABw = Y_WFCAM['aperMag3AverAB'] + 0.617
-J_ABw = J_WFCAM['aperMag3AverAB'] + 0.919 
-H_ABw = H_WFCAM['aperMag3AverAB'] + 1.379
-K_AB  = K_WFCAM['aperMag3AverAB'] + 1.900   
-
-## VIRCAM
-Z_ABv = Z_VIRCAM['aperMag3AverAB']  + 0.513 
-Y_ABv = Y_VIRCAM['aperMag3AverAB']  + 0.601
-J_ABv = J_VIRCAM['aperMag3AverAB']  + 0.921
-H_ABv = H_VIRCAM['aperMag3AverAB']  + 1.368
-Ks_AB = Ks_VIRCAM['aperMag3AverAB'] + 1.83
+##  V I R C A M   (from the Ross & Cross appendix) 
+#Z_ABv_all = Z_VIRCAM_all['aperMag3AverAB']  + 0.513 
+#Y_ABv_all = Y_VIRCAM_all['aperMag3AverAB']  + 0.601
+#J_ABv_all = J_VIRCAM_all['aperMag3AverAB']  + 0.921
+#H_ABv_all = H_VIRCAM_all['aperMag3AverAB']  + 1.368
+#Ks_AB_all = Ks_VIRCAM_all['aperMag3AverAB'] + 1.83
 
   
 ## WISE
@@ -96,13 +113,7 @@ Ks_AB = Ks_VIRCAM['aperMag3AverAB'] + 1.83
 ##
 ## http://matplotlib.org/examples/statistics/errorbar_limits.html
 ## http://matplotlib.org/examples/color/named_colors.html
-plt.rcParams.update({'font.size': 14})
-
-# 17.0 and 8.0 for paper
-#fig, ax = plt.subplots(figsize=(17.0, 8.0))
-# 12.0 and 8.0 for job app figure
-fig, ax = plt.subplots(figsize=(10.0, 6.0))
-
+fig, ax = plt.subplots(figsize=(8.0, 4.0))
 
 ## Adjusting the Whitespace for the plots
 left   = 0.14   # the left side of the subplots of the figure
@@ -117,41 +128,65 @@ plt.subplots_adjust(left=left, bottom=bottom, right=right, top=top, wspace=wspac
 ls              = 'solid'
 lw              = 1.0  # 2.4
 alpha           = 1.0
-fontsize        = 16
-labelsize       = fontsize
-tickwidth       = 2.0
+fontsize        = 14
+labelsize       = 14
 tickwidth       = 2.0
 ticklength      = 6.0
 ticklabelsize   = labelsize
 majorticklength = 12
 minorticklength = 6
+markeredgewidth = 1.5
 
 ## WSA
-ms = 6.
-#ax.scatter(Y_WFCAM['mjdObs'], Y_WFCAM['aperMag3AverAB'],   color='y'             )
-#ax.scatter(J_WFCAM['mjdObs'], J_WFCAM['aperMag3AverAB'],    color='goldenrod'    )
-#ax.scatter(H_WFCAM['mjdObs'], H_WFCAM['aperMag3AverAB'],   color='darkgoldenrod' )
-#ax.scatter(K_WFCAM['mjdObs'], K_WFCAM['aperMag3AverAB'],   color='orangered'    )
+ms = 3.
+#ax.scatter(Y_WFCAM_all['mjdObs'], Y_WFCAM_all['aperMag3AverAB'],   s=ms*10, color='y'             )
+#ax.scatter(J_WFCAM_all['mjdObs'], J_WFCAM_all['aperMag3AverAB'],   s=ms*10, color='goldenrod'    )
+#ax.scatter(H_WFCAM_all['mjdObs'], H_WFCAM_all['aperMag3AverAB'],   s=ms*10, color='darkgoldenrod' )
+#ax.scatter(K_WFCAM_all['mjdObs'], K_WFCAM_all['aperMag3AverAB'],   s=ms*10, color='orangered'    )
+#ax.errorbar(Y_WFCAM_all['mjdObs'], Y_WFCAM_all['aperMag3AverAB'],  yerr=Y_WFCAM_all['aperMag3Err'], fmt='h',  color='y',             ms=ms)
+#ax.errorbar(J_WFCAM_all['mjdObs'], J_WFCAM_all['aperMag3AverAB'],  yerr=J_WFCAM_all['aperMag3Err'], fmt='h',  color='goldenrod',     ms=ms)
+#ax.errorbar(H_WFCAM_all['mjdObs'], H_WFCAM_all['aperMag3AverAB'],  yerr=H_WFCAM_all['aperMag3Err'], fmt='h',  color='darkgoldenrod', ms=ms)
+#ax.errorbar(K_WFCAM_all['mjdObs'], K_WFCAM_all['aperMag3AverAB'],  yerr=K_WFCAM_all['aperMag3Err'], fmt='h',  color='orangered',     ms=ms)
 
-ms = 6.
-ax.errorbar(Y_WFCAM['mjdObs'], Y_WFCAM['aperMag3AverAB'],  yerr=Y_WFCAM['aperMag3Err'], fmt='h',  color='y',             ms=ms)
-ax.errorbar(J_WFCAM['mjdObs'], J_WFCAM['aperMag3AverAB'],  yerr=J_WFCAM['aperMag3Err'], fmt='h',  color='goldenrod',     ms=ms)
-ax.errorbar(H_WFCAM['mjdObs'], H_WFCAM['aperMag3AverAB'],  yerr=H_WFCAM['aperMag3Err'], fmt='h',  color='darkgoldenrod', ms=ms)
-ax.errorbar(K_WFCAM['mjdObs'], K_WFCAM['aperMag3AverAB'],  yerr=K_WFCAM['aperMag3Err'], fmt='h',  color='orangered',     ms=ms)
+ms = 7.
+#ax.errorbar(Y_WFCAM_30d['mjd'], Y_WFCAM_30d['aperMag3Ab'],  yerr=Y_WFCAM_30d['aperMag3Err'], label='', fmt='o',  color='k',             ms=ms*1.4)
+#ax.errorbar(J_WFCAM_30d['mjd'], J_WFCAM_30d['aperMag3Ab'],  yerr=J_WFCAM_30d['aperMag3Err'], label='',fmt='o',  color='k',             ms=ms*1.4)
+#ax.errorbar(H_WFCAM_30d['mjd'], H_WFCAM_30d['aperMag3Ab'],  yerr=H_WFCAM_30d['aperMag3Err'], label='',fmt='o',  color='k',             ms=ms*1.4)
+#ax.errorbar(K_WFCAM_30d['mjd'], K_WFCAM_30d['aperMag3Ab'],  yerr=K_WFCAM_30d['aperMag3Err'], label='',fmt='o',  color='k',             ms=ms*1.4)
+ax.errorbar(Y_WFCAM_30d['mjd'], Y_WFCAM_30d['aperMag3Ab'],  yerr=Y_WFCAM_30d['aperMag3Err'],
+            label='WFCAM Y', fmt='o', markeredgecolor ='black', markeredgewidth = markeredgewidth, color='yellow', ms=ms)
+ax.errorbar(J_WFCAM_30d['mjd'], J_WFCAM_30d['aperMag3Ab'],  yerr=J_WFCAM_30d['aperMag3Err'],
+            label='WFCAM J', fmt='o', markeredgecolor ='black', markeredgewidth = markeredgewidth, color='y',     ms=ms)
+ax.errorbar(H_WFCAM_30d['mjd'], H_WFCAM_30d['aperMag3Ab'],  yerr=H_WFCAM_30d['aperMag3Err'],
+            label='WFCAM H', fmt='o', markeredgecolor ='black', markeredgewidth = markeredgewidth, color='goldenrod',   ms=ms)
+ax.errorbar(K_WFCAM_30d['mjd'], K_WFCAM_30d['aperMag3Ab'],  yerr=K_WFCAM_30d['aperMag3Err'],
+            label='WFCAM K', fmt='o', markeredgecolor ='black', markeredgewidth = markeredgewidth, color='orangered',       ms=ms)
 
 
 ## VSA
-ms = 6.
-#ax.scatter(Y_VIRCAM['mjdObs'], Y_VIRCAM['aperMag3AverAB'],   color='y'             )
-#ax.scatter(J_VIRCAM['mjdObs'], J_VIRCAM['aperMag3AverAB'],    color='goldenrod'    )
-#ax.scatter(H_VIRCAM['mjdObs'], H_VIRCAM['aperMag3AverAB'],   color='darkgoldenrod' )
-#ax.scatter(Ks_VIRCAM['mjdObs'], Ks_VIRCAM['aperMag3AverAB'],   color='orangered'    )
+ms = 3.
+#ax.scatter( Y_VIRCAM_all['mjdObs'],  Y_VIRCAM_all['aperMag3AverAB'],  s=ms, color='y'             )
+#ax.scatter( J_VIRCAM_all['mjdObs'],  J_VIRCAM_all['aperMag3AverAB'],  s=ms,color='goldenrod'    )
+#ax.scatter( H_VIRCAM_all['mjdObs'],  H_VIRCAM_all['aperMag3AverAB'],  s=ms,color='darkgoldenrod' )
+#ax.scatter(Ks_VIRCAM_all['mjdObs'], Ks_VIRCAM_all['aperMag3AverAB'],  s=ms,color='orangered'    )
+#ax.errorbar(Y_VIRCAM_all['mjdObs'],  Y_VIRCAM_all['aperMag3AverAB'],  yerr=Y_VIRCAM_all['aperMag3Err'], fmt='h', color='y',             ms=ms)
+#ax.errorbar(J_VIRCAM_all['mjdObs'],  J_VIRCAM_all['aperMag3AverAB'],  yerr=J_VIRCAM_all['aperMag3Err'], fmt='h', color='goldenrod',     ms=ms)
+#ax.errorbar(H_VIRCAM_all['mjdObs'],  H_VIRCAM_all['aperMag3AverAB'],  yerr=H_VIRCAM_all['aperMag3Err'], fmt='h', color='darkgoldenrod', ms=ms)
+#ax.errorbar(Ks_VIRCAM_all['mjdObs'], Ks_VIRCAM_all['aperMag3AverAB'],  yerr=Ks_VIRCAM_all['aperMag3Err'], fmt='h',  color='orangered',   ms=ms)
 
 ms = 6.
-ax.errorbar(Y_VIRCAM['mjdObs'], Y_VIRCAM['aperMag3AverAB'],  yerr=Y_VIRCAM['aperMag3Err'], fmt='h', color='y',             ms=ms)
-ax.errorbar(J_VIRCAM['mjdObs'], J_VIRCAM['aperMag3AverAB'],  yerr=J_VIRCAM['aperMag3Err'], fmt='h', color='goldenrod',     ms=ms)
-ax.errorbar(H_VIRCAM['mjdObs'], H_VIRCAM['aperMag3AverAB'],  yerr=H_VIRCAM['aperMag3Err'], fmt='h', color='darkgoldenrod', ms=ms)
-ax.errorbar(Ks_VIRCAM['mjdObs'], Ks_VIRCAM['aperMag3AverAB'],  yerr=Ks_VIRCAM['aperMag3Err'], fmt='h',  color='orangered',   ms=ms)
+ax.errorbar(Y_VIRCAM_30d['mjd'],  Y_VIRCAM_30d['aperMag3Ab'],  yerr=Y_VIRCAM_30d['aperMag3Err'],
+            markeredgecolor ='black', markeredgewidth = markeredgewidth, label='VIRCAM Y',fmt='h', color='yellow',             ms=ms*1.4)
+ax.errorbar(J_VIRCAM_30d['mjd'],  J_VIRCAM_30d['aperMag3Ab'],  yerr=J_VIRCAM_30d['aperMag3Err'],
+            markeredgecolor ='black', markeredgewidth = markeredgewidth, label='VIRCAM J',fmt='h', color='y',     ms=ms*1.4)
+ax.errorbar(H_VIRCAM_30d['mjd'],  H_VIRCAM_30d['aperMag3Ab'],  yerr=H_VIRCAM_30d['aperMag3Err'],
+            markeredgecolor ='black', markeredgewidth = markeredgewidth, label='VIRCAM H',fmt='h', color='goldenrod', ms=ms*1.4)
+ax.errorbar(Ks_VIRCAM_30d['mjd'], Ks_VIRCAM_30d['aperMag3Ab'],  yerr=Ks_VIRCAM_30d['aperMag3Err'],
+            markeredgecolor ='black', markeredgewidth = markeredgewidth, label='VIRCAM Ks',fmt='h', color='orangered',    ms=ms*1.4)
+#ax.errorbar(Y_VIRCAM_30d['mjd'],  Y_VIRCAM_30d['aperMag3Ab'],  yerr=Y_VIRCAM_30d['aperMag3Err'],   label='',fmt='h', color='k',             ms=ms/1.4)
+#ax.errorbar(J_VIRCAM_30d['mjd'],  J_VIRCAM_30d['aperMag3Ab'],  yerr=J_VIRCAM_30d['aperMag3Err'],   label='',fmt='h', color='k',             ms=ms/1.4)
+#ax.errorbar(H_VIRCAM_30d['mjd'],  H_VIRCAM_30d['aperMag3Ab'],  yerr=H_VIRCAM_30d['aperMag3Err'],   label='',fmt='h', color='k',             ms=ms/1.4)
+#ax.errorbar(Ks_VIRCAM_30d['mjd'], Ks_VIRCAM_30d['aperMag3Ab'],  yerr=Ks_VIRCAM_30d['aperMag3Err'], label='',fmt='h', color='k',             ms=ms/1.4)
 
 
 
@@ -166,16 +201,16 @@ ms=4
 
 ## Tidy up the figure
 mjd_offset = 50.
-xmin = min(min(WSA_data['mjdObs']), min(VSA_data['mjdObs'])) - mjd_offset
-xmax = max(min(WSA_data['mjdObs']), max(VSA_data['mjdObs'])) + mjd_offset
+xmin = min(min(WSA_data_all['mjdObs']), min(VSA_data_all['mjdObs'])) - mjd_offset
+xmax = max(min(WSA_data_all['mjdObs']), max(VSA_data_all['mjdObs'])) + mjd_offset
 mag_offset = 0.5
-ymin = max(max(WSA_data['aperMag3AverAB']), max(VSA_data['aperMag3AverAB'])) + (mag_offset*1.25)
-ymax = min(max(WSA_data['aperMag3AverAB']), min(VSA_data['aperMag3AverAB'])) - (mag_offset*2.0)
+ymin = max(max(WSA_data_all['aperMag3AverAB']), max(VSA_data_all['aperMag3AverAB'])) + (mag_offset*1.25)
+ymax = min(max(WSA_data_all['aperMag3AverAB']), min(VSA_data_all['aperMag3AverAB'])) - (mag_offset*2.0)
 
 ax.set_xlim((xmin, xmax))
 ax.set_ylim((ymin, ymax))
-ax.tick_params('x', direction='in')
-ax.tick_params('y', direction='in')
+ax.tick_params('x', direction='in', labelsize=ticklabelsize )
+ax.tick_params('y', direction='in', labelsize=ticklabelsize)
 #ax.minorticks_on('x', direction='in')
 #ay.minorticks_on()
 #ax.get_xaxis().set_tick_params(which='both', direction='out')
@@ -183,17 +218,11 @@ ax.tick_params('y', direction='in')
 #ax.grid(True)
 
 ## https://matplotlib.org/api/legend_api.html
-plt.legend([
-            'WFCAM Y', 'WFCAM J', 'WFCAM H', 'WFCAM K',
-             'VIRCAM Y', 'VIRCAM J', 'VIRCAM H', 'VIRCAM Ks', ],
-    #        'WISE W1', 'WISE W2'  ],
-#           loc="lower left", ncol=3, shadow=True, fancybox=True,
-           loc="upper left", ncol=3, shadow=True, fancybox=True,
-           fontsize=16, frameon=True)
+plt.legend(loc="upper left", ncol=2, fontsize=labelsize/1.4, frameon=True)
 
-plt.title(inputQuasar, fontsize=fontsize*1.6)
-plt.xlabel('MJD')
-plt.ylabel('magnitude (AB)')
+plt.title(inputQuasar,         fontsize=fontsize)
+plt.xlabel('MJD',              fontsize=fontsize)
+plt.ylabel('magnitude (AB)',   fontsize=fontsize)
 
 ##plt.show()
 #fig.savefig("plot.pdf",)
