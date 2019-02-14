1b663568f78a4207b93463b5c57ef931ea58bbbc
npross <npross@lbl.gov>
2019-02-14 21:39:50 +0000

    light_curves plotting update

--- light_curves/lightcurve_plot.py
+++ light_curves/lightcurve_plot.py
@@ -2,17 +2,17 @@
 Why doesn't matplotlib look as good as SM??!!!
 http://space.mit.edu/home/turnerm/python.html
 '''
-
 import math
 import numpy as np
-import inquirer
 import matplotlib.pyplot as plt
 
-from matplotlib import colors as mcolors
 from astropy.io import ascii
+from matplotlib import colors as mcolors
+from matplotlib.font_manager import FontProperties
+
 
 ## Setting up the path for the input data
-path = '/cos_pc19a_npr/programs/quasars/highest_z/data/LightCurves/'
+path = '/cos_pc19a_npr/programs/quasars/highest_z/data/light_curves/'
 
 choice = '1'
 ## Ordered by qsoID number. 
@@ -22,14 +22,23 @@ print('  2)   CFHQSJ0216-045     (qsoID  56; faint)')
 print('  3)   SHELLQsJ0220-0432  (qsoID  59; faint)') 
 print('  4)   SDSSJ0836+0054     (qsoID 121)')
 print('  5)   SDSSJ0959+0227     (qsoID 162; faint)')
+print('  6)   SDSSJ1000+0234     (qsoID 164; faint)')
+print('  7)   SHELLQsJ1146+0124  (qsoID 206)')
+print('  8)   VIKJ1148+0056      (qsoID 214)')
+print('  9)   SDSSJ2216+0013     (qsoID 337)')
 print()
-#choice = input('Which object? ')
+choice = input('Which object? ')
 
 if choice == '1' : inputQuasar = 'MMTJ0215-0529'
 if choice == '2' : inputQuasar = 'CFHQSJ0216-0455'
 if choice == '3' : inputQuasar = 'SHELLQsJ0220-0432'
 if choice == '4' : inputQuasar = 'SDSSJ0836+0054'
 if choice == '5' : inputQuasar = 'SDSSJ0959+0227'
+if choice == '6' : inputQuasar = 'SDSSJ1000+0234'
+if choice == '7' : inputQuasar = 'SDSSJ0959+0227'
+if choice == '8' : inputQuasar = 'SHELLQsJ1146+0124'
+if choice == '9' : inputQuasar = 'SDSSJ2216+0013'
+    
 
 print
 print(" Plotting W/VSA light curve for ", inputQuasar)
@@ -48,6 +57,13 @@ WSA_data_30d = ascii.read(path+inputQuasar+'/WSA_lc_30d.dat')
 ## VSA - VISTA Science Archive
 VSA_data_all = ascii.read(path+inputQuasar+'/VSA_lc_all.dat')
 VSA_data_30d = ascii.read(path+inputQuasar+'/VSA_lc_30d.dat')
+
+## NEOWISE-R
+NEOWISER =  ascii.read(path+inputQuasar+'/NEOWISER-R_SingleExp_L1b.dat')
+#WISE_W1 = ascii.read(path+'WISE_W1_LC.dat')
+#WISE_W2 = ascii.read(path+'WISE_W2_LC.dat')
+#WISE_L1bs = ascii.read(path+'J110057_l1b.tbl')
+
 
 ##  Assigned in WSA: 1=Z,2=Y,3=J,4=H,5=K,6=H2,7=Br,8=blank)
 Z_WFCAM_all = WSA_data_all[(WSA_data_all['filterID'] == 1) & (WSA_data_all['aperMag3AverAB']>0)]
@@ -76,24 +92,9 @@ H_VIRCAM_30d  = VSA_data_30d[(VSA_data_30d['filterID'] == 4) & (VSA_data_30d['ap
 Ks_VIRCAM_30d = VSA_data_30d[(VSA_data_30d['filterID'] == 5) & (VSA_data_30d['aperMag3Ab']>0)]
 
 
-##  V I R C A M   (from the Ross & Cross appendix) 
-#Z_ABv_all = Z_VIRCAM_all['aperMag3AverAB']  + 0.513 
-#Y_ABv_all = Y_VIRCAM_all['aperMag3AverAB']  + 0.601
-#J_ABv_all = J_VIRCAM_all['aperMag3AverAB']  + 0.921
-#H_ABv_all = H_VIRCAM_all['aperMag3AverAB']  + 1.368
-#Ks_AB_all = Ks_VIRCAM_all['aperMag3AverAB'] + 1.83
-
-  
-## WISE
-#WISE_W1 = ascii.read(path+'WISE_W1_LC.dat')
-#WISE_W2 = ascii.read(path+'WISE_W2_LC.dat')
-#WISE_L1bs = ascii.read(path+'J110057_l1b.tbl')
-
 ## For WISE, we adopt 2.699 and 3.339 as the conversions to AB from W1 and W2 Vega magnitudes,
-#WISE_W1_ABave = WISE_W1['W1_vega'] + 2.699
-#WISE_W2_ABave = WISE_W2['W2_vega'] + 3.339
-#WISE_W1_ABs   = WISE_L1bs['w1mpro'] + 2.699
-#WISE_W2_ABs   = WISE_L1bs['w2mpro'] + 3.339
+WISE_W1_AB = NEOWISER['w1mpro'] + 2.699
+WISE_W2_AB = NEOWISER['w2mpro'] + 3.339
 
 
 
@@ -102,7 +103,7 @@ Ks_VIRCAM_30d = VSA_data_30d[(VSA_data_30d['filterID'] == 5) & (VSA_data_30d['ap
 ##
 ## http://matplotlib.org/examples/statistics/errorbar_limits.html
 ## http://matplotlib.org/examples/color/named_colors.html
-fig, ax = plt.subplots(figsize=(8.0, 4.0))
+fig, ax = plt.subplots(figsize=(8.0, 6.0))
 
 ## Adjusting the Whitespace for the plots
 left   = 0.14   # the left side of the subplots of the figure
@@ -128,73 +129,70 @@ markeredgewidth = 1.5
 
 ## WSA
 ms = 3.
-#ax.scatter(Y_WFCAM_all['mjdObs'], Y_WFCAM_all['aperMag3AverAB'],   s=ms*10, color='y'             )
-#ax.scatter(J_WFCAM_all['mjdObs'], J_WFCAM_all['aperMag3AverAB'],   s=ms*10, color='goldenrod'    )
-#ax.scatter(H_WFCAM_all['mjdObs'], H_WFCAM_all['aperMag3AverAB'],   s=ms*10, color='darkgoldenrod' )
-#ax.scatter(K_WFCAM_all['mjdObs'], K_WFCAM_all['aperMag3AverAB'],   s=ms*10, color='orangered'    )
-#ax.errorbar(Y_WFCAM_all['mjdObs'], Y_WFCAM_all['aperMag3AverAB'],  yerr=Y_WFCAM_all['aperMag3Err'], fmt='h',  color='y',             ms=ms)
-#ax.errorbar(J_WFCAM_all['mjdObs'], J_WFCAM_all['aperMag3AverAB'],  yerr=J_WFCAM_all['aperMag3Err'], fmt='h',  color='goldenrod',     ms=ms)
-#ax.errorbar(H_WFCAM_all['mjdObs'], H_WFCAM_all['aperMag3AverAB'],  yerr=H_WFCAM_all['aperMag3Err'], fmt='h',  color='darkgoldenrod', ms=ms)
-#ax.errorbar(K_WFCAM_all['mjdObs'], K_WFCAM_all['aperMag3AverAB'],  yerr=K_WFCAM_all['aperMag3Err'], fmt='h',  color='orangered',     ms=ms)
+ax.scatter(Z_WFCAM_all['mjdObs'], Z_WFCAM_all['aperMag3AverAB'], label='', s=ms*10, color='olive'             )
+ax.scatter(Y_WFCAM_all['mjdObs'], Y_WFCAM_all['aperMag3AverAB'], label='', s=ms*10, color='orange'             )
+ax.scatter(J_WFCAM_all['mjdObs'], J_WFCAM_all['aperMag3AverAB'], label='', s=ms*10, color='goldenrod'    )
+ax.scatter(H_WFCAM_all['mjdObs'], H_WFCAM_all['aperMag3AverAB'], label='', s=ms*10, color='y' )
+ax.scatter(K_WFCAM_all['mjdObs'], K_WFCAM_all['aperMag3AverAB'], label='', s=ms*10, color='yellow'    )
 
 ms = 7.
-#ax.errorbar(Y_WFCAM_30d['mjd'], Y_WFCAM_30d['aperMag3Ab'],  yerr=Y_WFCAM_30d['aperMag3Err'], label='', fmt='o',  color='k',             ms=ms*1.4)
-#ax.errorbar(J_WFCAM_30d['mjd'], J_WFCAM_30d['aperMag3Ab'],  yerr=J_WFCAM_30d['aperMag3Err'], label='',fmt='o',  color='k',             ms=ms*1.4)
-#ax.errorbar(H_WFCAM_30d['mjd'], H_WFCAM_30d['aperMag3Ab'],  yerr=H_WFCAM_30d['aperMag3Err'], label='',fmt='o',  color='k',             ms=ms*1.4)
-#ax.errorbar(K_WFCAM_30d['mjd'], K_WFCAM_30d['aperMag3Ab'],  yerr=K_WFCAM_30d['aperMag3Err'], label='',fmt='o',  color='k',             ms=ms*1.4)
+ax.errorbar(Z_WFCAM_30d['mjd'], Z_WFCAM_30d['aperMag3Ab'],  yerr=Z_WFCAM_30d['aperMag3Err'],
+            label='WFCAM Z', fmt='o', markeredgecolor ='black', markeredgewidth = markeredgewidth, color='olive', ms=ms)
 ax.errorbar(Y_WFCAM_30d['mjd'], Y_WFCAM_30d['aperMag3Ab'],  yerr=Y_WFCAM_30d['aperMag3Err'],
-            label='WFCAM Y', fmt='o', markeredgecolor ='black', markeredgewidth = markeredgewidth, color='yellow', ms=ms)
+            label='WFCAM Y', fmt='o', markeredgecolor ='black', markeredgewidth = markeredgewidth, color='orange', ms=ms)
 ax.errorbar(J_WFCAM_30d['mjd'], J_WFCAM_30d['aperMag3Ab'],  yerr=J_WFCAM_30d['aperMag3Err'],
-            label='WFCAM J', fmt='o', markeredgecolor ='black', markeredgewidth = markeredgewidth, color='y',     ms=ms)
+            label='WFCAM J', fmt='o', markeredgecolor ='black', markeredgewidth = markeredgewidth, color='goldenrod',     ms=ms)
 ax.errorbar(H_WFCAM_30d['mjd'], H_WFCAM_30d['aperMag3Ab'],  yerr=H_WFCAM_30d['aperMag3Err'],
-            label='WFCAM H', fmt='o', markeredgecolor ='black', markeredgewidth = markeredgewidth, color='goldenrod',   ms=ms)
+            label='WFCAM H', fmt='o', markeredgecolor ='black', markeredgewidth = markeredgewidth, color='y',   ms=ms)
 ax.errorbar(K_WFCAM_30d['mjd'], K_WFCAM_30d['aperMag3Ab'],  yerr=K_WFCAM_30d['aperMag3Err'],
-            label='WFCAM K', fmt='o', markeredgecolor ='black', markeredgewidth = markeredgewidth, color='orangered',       ms=ms)
-
+            label='WFCAM K', fmt='o', markeredgecolor ='black', markeredgewidth = markeredgewidth, color='yellow',       ms=ms)
 
 ## VSA
 ms = 3.
-#ax.scatter( Y_VIRCAM_all['mjdObs'],  Y_VIRCAM_all['aperMag3AverAB'],  s=ms, color='y'             )
-#ax.scatter( J_VIRCAM_all['mjdObs'],  J_VIRCAM_all['aperMag3AverAB'],  s=ms,color='goldenrod'    )
-#ax.scatter( H_VIRCAM_all['mjdObs'],  H_VIRCAM_all['aperMag3AverAB'],  s=ms,color='darkgoldenrod' )
-#ax.scatter(Ks_VIRCAM_all['mjdObs'], Ks_VIRCAM_all['aperMag3AverAB'],  s=ms,color='orangered'    )
-#ax.errorbar(Y_VIRCAM_all['mjdObs'],  Y_VIRCAM_all['aperMag3AverAB'],  yerr=Y_VIRCAM_all['aperMag3Err'], fmt='h', color='y',             ms=ms)
-#ax.errorbar(J_VIRCAM_all['mjdObs'],  J_VIRCAM_all['aperMag3AverAB'],  yerr=J_VIRCAM_all['aperMag3Err'], fmt='h', color='goldenrod',     ms=ms)
-#ax.errorbar(H_VIRCAM_all['mjdObs'],  H_VIRCAM_all['aperMag3AverAB'],  yerr=H_VIRCAM_all['aperMag3Err'], fmt='h', color='darkgoldenrod', ms=ms)
-#ax.errorbar(Ks_VIRCAM_all['mjdObs'], Ks_VIRCAM_all['aperMag3AverAB'],  yerr=Ks_VIRCAM_all['aperMag3Err'], fmt='h',  color='orangered',   ms=ms)
+ax.scatter( Z_VIRCAM_all['mjdObs'],  Z_VIRCAM_all['aperMag3AverAB'], label='', s=ms, color='olive')
+ax.scatter( Y_VIRCAM_all['mjdObs'],  Y_VIRCAM_all['aperMag3AverAB'], label='', s=ms, color='orange')
+ax.scatter( J_VIRCAM_all['mjdObs'],  J_VIRCAM_all['aperMag3AverAB'], label='', s=ms, color='goldenrod')
+ax.scatter( H_VIRCAM_all['mjdObs'],  H_VIRCAM_all['aperMag3AverAB'], label='', s=ms, color='y')
+ax.scatter(Ks_VIRCAM_all['mjdObs'], Ks_VIRCAM_all['aperMag3AverAB'], label='', s=ms, color='yellow')
 
-ms = 6.
+ms = 7.
 ax.errorbar(Y_VIRCAM_30d['mjd'],  Y_VIRCAM_30d['aperMag3Ab'],  yerr=Y_VIRCAM_30d['aperMag3Err'],
-            markeredgecolor ='black', markeredgewidth = markeredgewidth, label='VIRCAM Y',fmt='h', color='yellow',             ms=ms*1.4)
+            markeredgecolor ='black', markeredgewidth = markeredgewidth, fmt='h', label='VIRCAM Z',  color='olive', ms=ms*1.4)
+ax.errorbar(Y_VIRCAM_30d['mjd'],  Y_VIRCAM_30d['aperMag3Ab'],  yerr=Y_VIRCAM_30d['aperMag3Err'],
+            markeredgecolor ='black', markeredgewidth = markeredgewidth, fmt='h', label='VIRCAM Y',  color='orange', ms=ms*1.4)
 ax.errorbar(J_VIRCAM_30d['mjd'],  J_VIRCAM_30d['aperMag3Ab'],  yerr=J_VIRCAM_30d['aperMag3Err'],
-            markeredgecolor ='black', markeredgewidth = markeredgewidth, label='VIRCAM J',fmt='h', color='y',     ms=ms*1.4)
+            markeredgecolor ='black', markeredgewidth = markeredgewidth, fmt='h', label='VIRCAM J',  color='goldenrod',         ms=ms*1.4)
 ax.errorbar(H_VIRCAM_30d['mjd'],  H_VIRCAM_30d['aperMag3Ab'],  yerr=H_VIRCAM_30d['aperMag3Err'],
-            markeredgecolor ='black', markeredgewidth = markeredgewidth, label='VIRCAM H',fmt='h', color='goldenrod', ms=ms*1.4)
+            markeredgecolor ='black', markeredgewidth = markeredgewidth, fmt='h', label='VIRCAM H',  color='y', ms=ms*1.4)
 ax.errorbar(Ks_VIRCAM_30d['mjd'], Ks_VIRCAM_30d['aperMag3Ab'],  yerr=Ks_VIRCAM_30d['aperMag3Err'],
-            markeredgecolor ='black', markeredgewidth = markeredgewidth, label='VIRCAM Ks',fmt='h', color='orangered',    ms=ms*1.4)
-#ax.errorbar(Y_VIRCAM_30d['mjd'],  Y_VIRCAM_30d['aperMag3Ab'],  yerr=Y_VIRCAM_30d['aperMag3Err'],   label='',fmt='h', color='k',             ms=ms/1.4)
-#ax.errorbar(J_VIRCAM_30d['mjd'],  J_VIRCAM_30d['aperMag3Ab'],  yerr=J_VIRCAM_30d['aperMag3Err'],   label='',fmt='h', color='k',             ms=ms/1.4)
-#ax.errorbar(H_VIRCAM_30d['mjd'],  H_VIRCAM_30d['aperMag3Ab'],  yerr=H_VIRCAM_30d['aperMag3Err'],   label='',fmt='h', color='k',             ms=ms/1.4)
-#ax.errorbar(Ks_VIRCAM_30d['mjd'], Ks_VIRCAM_30d['aperMag3Ab'],  yerr=Ks_VIRCAM_30d['aperMag3Err'], label='',fmt='h', color='k',             ms=ms/1.4)
-
-
+            markeredgecolor ='black', markeredgewidth = markeredgewidth, fmt='h', label='VIRCAM Ks', color='yellow',ms=ms*1.4)
 
 ## WISE W1/W2
-ms=16
-#ax.errorbar(WISE_W1['MJD'], WISE_W1_ABave, yerr=WISE_W1['W1_unc'], fmt='o', ms=ms, linestyle=ls, linewidth=lw*2.5, color='indigo')
-#ax.errorbar(WISE_W2['MJD'], WISE_W2_ABave, yerr=WISE_W2['W2_unc'], fmt='o', ms=ms, linestyle=ls, linewidth=lw*2.5, color='brown')
-ms=4
+ms=10
+ax.errorbar(NEOWISER['mjd'], WISE_W1_AB, yerr=NEOWISER['w1sigmpro'], fmt='o', ms=ms, label='NEOWISE-R W1',
+            markeredgecolor ='black', markeredgewidth = markeredgewidth, color='peru') #linestyle=ls, linewidth=lw*2.5, 
+ax.errorbar(NEOWISER['mjd'], WISE_W2_AB, yerr=NEOWISER['w2sigmpro'], fmt='o', ms=ms, label='NEOWISE-R W2', 
+            markeredgecolor ='black', markeredgewidth = markeredgewidth, color='orangered') #linestyle=ls, linewidth=lw*2.5, 
+#ms=4
 #ax.errorbar(WISE_L1bs['mjd'], WISE_W1_ABs, yerr=WISE_L1bs['w1sigmpro'], fmt='o', ms=ms, color='indigo')
 #ax.errorbar(WISE_L1bs['mjd'], WISE_W2_ABs, yerr=WISE_L1bs['w2sigmpro'], fmt='o', ms=ms, color='brown')
 
+#alignment = {'horizontalalignment': 'center', 'verticalalignment': 'baseline'}
+#font0 = FontProperties()
+#font1 = font0.copy()
+#font1.set_size('large')
+#plt.text(-0.8, 0.9, 'family', fontproperties=font1)
 
 ## Tidy up the figure
 mjd_offset = 50.
 xmin = min(min(WSA_data_all['mjdObs']), min(VSA_data_all['mjdObs'])) - mjd_offset
 xmax = max(min(WSA_data_all['mjdObs']), max(VSA_data_all['mjdObs'])) + mjd_offset
+xmax = 58484 ## 2019-Jan-01
 mag_offset = 0.5
 ymin = max(max(WSA_data_all['aperMag3AverAB']), max(VSA_data_all['aperMag3AverAB'])) + (mag_offset*1.00)
 ymax = min(max(WSA_data_all['aperMag3AverAB']), min(VSA_data_all['aperMag3AverAB'])) - (mag_offset*2.25)
+#ymin = 27.4
+ymax = 16.8
 
 ax.set_xlim((xmin, xmax))
 ax.set_ylim((ymin, ymax))
