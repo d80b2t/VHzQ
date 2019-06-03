1b530597770b4578e8594d2e504a6c9271c964ce
npross <npross@lbl.gov>
2019-06-03 14:08:27 +0100

    SEDs/filters_vs_QSOstars.py and Figure 1 update

--- SEDs/filters_vs_QSOstars.py
+++ SEDs/filters_vs_QSOstars.py
@@ -79,6 +79,13 @@ CFHT_Wband = ascii.read(table)
 CFHT_Wband_wave = CFHT_Wband['col1']
 CFHT_Wband_flux = CFHT_Wband['col2']
 
+##     2MASS
+infile = '2MASS/2MASS_Ks.dat'
+table = path+infile
+TwoMASS_Ksband      = ascii.read(table)
+TwoMASS_Ksband_wave = TwoMASS_Ksband['wavelength']
+TwoMASS_Ksband_thru = TwoMASS_Ksband['throughput']
+
 ##      UKIRT
 infile = 'UKIRT/UKIRT_Z.dat'
 table = path+infile
@@ -106,12 +113,33 @@ UKIRT_Kband      = ascii.read(table)
 UKIRT_Kband_wave = UKIRT_Kband['wavelength']
 UKIRT_Kband_thru = UKIRT_Kband['throughput']
 
-##     2MASS
-infile = '2MASS/2MASS_Ks.dat'
+
+##  VIRCAM/VISTA  
+infile = 'VIRCAM/Paranal_VISTA.Z.dat'
 table = path+infile
-TwoMASS_Ksband      = ascii.read(table)
-TwoMASS_Ksband_wave = TwoMASS_Ksband['wavelength']
-TwoMASS_Ksband_thru = TwoMASS_Ksband['throughput']
+VISTA_Zband      = ascii.read(table)
+VISTA_Zband_wave = VISTA_Zband['wavelength']
+VISTA_Zband_thru = VISTA_Zband['throughput']
+infile = 'VIRCAM/Paranal_VISTA.Y.dat'
+table = path+infile
+VISTA_Yband      = ascii.read(table)
+VISTA_Yband_wave = VISTA_Yband['wavelength']
+VISTA_Yband_thru = VISTA_Yband['throughput']
+infile = 'VIRCAM/Paranal_VISTA.J.dat'
+table = path+infile
+VISTA_Jband      = ascii.read(table)
+VISTA_Jband_wave = VISTA_Jband['wavelength']
+VISTA_Jband_thru = VISTA_Jband['throughput']
+infile = 'VIRCAM/Paranal_VISTA.H.dat'
+table = path+infile
+VISTA_Hband      = ascii.read(table)
+VISTA_Hband_wave = VISTA_Hband['wavelength']
+VISTA_Hband_thru = VISTA_Hband['throughput']
+infile = 'VIRCAM/Paranal_VISTA.Ks.dat'
+table = path+infile
+VISTA_Ksband      = ascii.read(table)
+VISTA_Ksband_wave = VISTA_Ksband['wavelength']
+VISTA_Ksband_thru = VISTA_Ksband['throughput']
 
 ##     WISE
 infile = 'WISE/RSR-W1.txt'
@@ -209,6 +237,12 @@ T4pnt5_wave = T4pnt5_2MASS_0559['wavelength']
 T4pnt5_flux = T4pnt5_2MASS_0559['flux']
 
 
+plot_DECam     = 'N'
+plot_PanSTARRS = 'N' 
+plot_UKIRT     = 'y'
+plot_VISTA     = 'n'
+plot_WISE      = 'y'
+
 ##
 ## Making the plot...
 ##
@@ -219,11 +253,12 @@ fig, ax = plt.subplots(figsize=(16, 7))
 ax.set_xscale('log')
 #ax.set_yscale('log')
 
-xmin =   0.38   ## in um, starting with g-band
-xmin =   0.52   ## in um, starting with r-band
-xmax =  5.5     ## with WISE W2
+#xmin =   0.38   ## in um, starting with g-band
+#xmin =   0.52   ## in um, starting with r-band
+xmin =   0.72   ## in um, starting with Z-band
+#xmax =  5.5    ## with WISE W2
 #xmax = 18.5    ## with WISE W3
-#xmax = 28.00   ## with WISE W4
+xmax = 28.00    ## with WISE W4
 ymin =  0.00
 ymax =  1.30
 #ymin = 0.001   ## for y-log,
@@ -248,96 +283,112 @@ minorticklength = 6
 ##
 ## FILTERS
 ##
-##   D E C a m
-#ax.plot( (DES_gband_wave/10000.),  (DES_gband_thru/100.), color='violet',         alpha=alpha, linewidth=linewidth)
-#ax.plot( (DES_rband_wave/10000.),  (DES_rband_thru/100.), color='indigo',         alpha=alpha, linewidth=linewidth)
-#ax.plot( (DES_iband_wave/10000.),  (DES_iband_thru/100.), color='royalblue',      alpha=alpha, linewidth=linewidth)
-#ax.plot( (DES_zband_wave/10000.),  (DES_zband_thru/100.), color='deepskyblue',    alpha=alpha, linewidth=linewidth)
-#ax.plot( (DES_Yband_wave/10000.),  (DES_Yband_thru/100.), color='paleturquoise',  alpha=alpha, linewidth=linewidth)
-#ax.fill( (DES_gband_wave/10000.),  (DES_gband_thru/100.), color='violet',         alpha=alpha/2)
-#ax.fill( (DES_rband_wave/10000.),  (DES_rband_thru/100.), color='indigo',         alpha=alpha/2)
-#ax.fill( (DES_iband_wave/10000.),  (DES_iband_thru/100.), color='royalblue',      alpha=alpha/2)
-#ax.fill( (DES_zband_wave/10000.),  (DES_zband_thru/100.), color='deepskyblue',    alpha=alpha/2)
-#ax.fill( (DES_Yband_wave/10000.),  (DES_Yband_thru/100.), color='paleturquoise',  alpha=alpha/2)
+if plot_DECam == 'y':
+    ##   D E C a m
+    ax.plot( (DES_gband_wave/10000.),  (DES_gband_thru/100.), color='violet',         alpha=alpha, linewidth=linewidth)
+    ax.plot( (DES_rband_wave/10000.),  (DES_rband_thru/100.), color='indigo',         alpha=alpha, linewidth=linewidth)
+    ax.plot( (DES_iband_wave/10000.),  (DES_iband_thru/100.), color='royalblue',      alpha=alpha, linewidth=linewidth)
+    ax.plot( (DES_zband_wave/10000.),  (DES_zband_thru/100.), color='deepskyblue',    alpha=alpha, linewidth=linewidth)
+    ax.plot( (DES_Yband_wave/10000.),  (DES_Yband_thru/100.), color='paleturquoise',  alpha=alpha, linewidth=linewidth)
+    ax.fill( (DES_gband_wave/10000.),  (DES_gband_thru/100.), color='violet',         alpha=alpha/2)
+    ax.fill( (DES_rband_wave/10000.),  (DES_rband_thru/100.), color='indigo',         alpha=alpha/2)
+    ax.fill( (DES_iband_wave/10000.),  (DES_iband_thru/100.), color='royalblue',      alpha=alpha/2)
+    ax.fill( (DES_zband_wave/10000.),  (DES_zband_thru/100.), color='deepskyblue',    alpha=alpha/2)
+    ax.fill( (DES_Yband_wave/10000.),  (DES_Yband_thru/100.), color='paleturquoise',  alpha=alpha/2)
 
-## Pan-STARRS
-#ax.plot( (PanSTARRS_wave/1000.),  (PanSTARRS_gp1/PanSTARRS_gp1.max()), color='darkviolet',      alpha=alpha, linewidth=linewidth)
-#ax.fill( (PanSTARRS_wave/1000.),  (PanSTARRS_gp1/PanSTARRS_gp1.max()), color='darkviolet',      alpha=alpha/2)
-ax.plot( (PanSTARRS_wave/1000.),  (PanSTARRS_rp1/PanSTARRS_rp1.max()), color='slateblue',      alpha=alpha, linewidth=linewidth)
-ax.fill( (PanSTARRS_wave/1000.),  (PanSTARRS_rp1/PanSTARRS_rp1.max()), color='slateblue',      alpha=alpha/2)
-ax.plot( (PanSTARRS_wave/1000.),  (PanSTARRS_ip1/PanSTARRS_ip1.max()), color='dodgerblue',      alpha=alpha, linewidth=linewidth)
-ax.fill( (PanSTARRS_wave/1000.),  (PanSTARRS_ip1/PanSTARRS_ip1.max()), color='dodgerblue',      alpha=alpha/2)
-ax.plot( (PanSTARRS_wave/1000.),  (PanSTARRS_zp1/PanSTARRS_zp1.max()), color='skyblue',         alpha=alpha, linewidth=linewidth)
-ax.fill( (PanSTARRS_wave/1000.),  (PanSTARRS_zp1/PanSTARRS_zp1.max()), color='skyblue',         alpha=alpha/2)
-ax.plot( (PanSTARRS_wave/1000.),  (PanSTARRS_yp1/PanSTARRS_yp1.max()), color='mediumturquoise', alpha=alpha, linewidth=linewidth)
-ax.fill( (PanSTARRS_wave/1000.),  (PanSTARRS_yp1/PanSTARRS_yp1.max()), color='mediumturquoise', alpha=alpha/2)
+if plot_PanSTARRS == 'y':
+    ## Pan-STARRS
+    ax.plot( (PanSTARRS_wave/1000.),  (PanSTARRS_gp1/PanSTARRS_gp1.max()), color='darkviolet',      alpha=alpha, linewidth=linewidth)
+    ax.fill( (PanSTARRS_wave/1000.),  (PanSTARRS_gp1/PanSTARRS_gp1.max()), color='darkviolet',      alpha=alpha/2)
+    ax.plot( (PanSTARRS_wave/1000.),  (PanSTARRS_rp1/PanSTARRS_rp1.max()), color='slateblue',      alpha=alpha, linewidth=linewidth)
+    ax.fill( (PanSTARRS_wave/1000.),  (PanSTARRS_rp1/PanSTARRS_rp1.max()), color='slateblue',      alpha=alpha/2)
+    ax.plot( (PanSTARRS_wave/1000.),  (PanSTARRS_ip1/PanSTARRS_ip1.max()), color='dodgerblue',      alpha=alpha, linewidth=linewidth)
+    ax.fill( (PanSTARRS_wave/1000.),  (PanSTARRS_ip1/PanSTARRS_ip1.max()), color='dodgerblue',      alpha=alpha/2)
+    ax.plot( (PanSTARRS_wave/1000.),  (PanSTARRS_zp1/PanSTARRS_zp1.max()), color='skyblue',         alpha=alpha, linewidth=linewidth)
+    ax.fill( (PanSTARRS_wave/1000.),  (PanSTARRS_zp1/PanSTARRS_zp1.max()), color='skyblue',         alpha=alpha/2)
+    ax.plot( (PanSTARRS_wave/1000.),  (PanSTARRS_yp1/PanSTARRS_yp1.max()), color='mediumturquoise', alpha=alpha, linewidth=linewidth)
+    ax.fill( (PanSTARRS_wave/1000.),  (PanSTARRS_yp1/PanSTARRS_yp1.max()), color='mediumturquoise', alpha=alpha/2)
 
-## for xmin=0.38
-#plt.text(0.460, 1.1, r'g', color ='darkviolet',      fontsize=fontsize)
-#plt.text(0.570, 1.1, r'r', color ='slateblue',       fontsize=fontsize)
-#plt.text(0.700, 1.1, r'i', color ='dodgerblue',      fontsize=fontsize)
-#plt.text(0.800, 1.1, r'z', color ='skyblue',         fontsize=fontsize)
-#plt.text(0.900, 1.1, r'y', color ='mediumturquoise', fontsize=fontsize)
+    ## for xmin=0.38
+    plt.text(0.460, 1.1, r'g', color ='darkviolet',      fontsize=fontsize)
+    plt.text(0.570, 1.1, r'r', color ='slateblue',       fontsize=fontsize)
+    plt.text(0.700, 1.1, r'i', color ='dodgerblue',      fontsize=fontsize)
+    plt.text(0.800, 1.1, r'z', color ='skyblue',         fontsize=fontsize)
+    plt.text(0.900, 1.1, r'y', color ='mediumturquoise', fontsize=fontsize)
 
 ## for xmin=0.52
-plt.text(0.590, 1.1, r'r', color ='slateblue',       fontsize=fontsize, style='italic', weight='bold')
-plt.text(0.710, 1.1, r'i', color ='dodgerblue',      fontsize=fontsize, style='italic', weight='bold')
-plt.text(0.840, 1.1, r'z', color ='skyblue',         fontsize=fontsize, style='italic', weight='bold')
-plt.text(0.910, 1.1, r'y', color ='mediumturquoise', fontsize=fontsize, style='italic', weight='bold')
-
+#plt.text(0.590, 1.1, r'r', color ='slateblue',       fontsize=fontsize, style='italic', weight='bold')
+#plt.text(0.710, 1.1, r'i', color ='dodgerblue',      fontsize=fontsize, style='italic', weight='bold')
+#plt.text(0.840, 1.1, r'z', color ='skyblue',         fontsize=fontsize, style='italic', weight='bold')
+#plt.text(0.910, 1.1, r'y', color ='mediumturquoise', fontsize=fontsize, style='italic', weight='bold')
 
 ## CFHT
-ax.plot( (CFHT_Wband_wave/1000.),  (CFHT_Wband_flux/CFHT_Wband_flux.max()), color='silver',   alpha=alpha, linewidth=linewidth)
-ax.fill( (CFHT_Wband_wave/1000.),  (CFHT_Wband_flux/CFHT_Wband_flux.max()), color='silver',   alpha=alpha/2)
-plt.text(1.400, 1.1, r'W', color ='silver', fontsize=fontsize)
+#ax.plot( (CFHT_Wband_wave/1000.),  (CFHT_Wband_flux/CFHT_Wband_flux.max()), color='silver',   alpha=alpha, linewidth=linewidth)
+#ax.fill( (CFHT_Wband_wave/1000.),  (CFHT_Wband_flux/CFHT_Wband_flux.max()), color='silver',   alpha=alpha/2)
+#plt.text(1.400, 1.1, r'W', color ='silver', fontsize=fontsize)
 
-## UKIRT
-#ax.plot( (UKIRT_Zband_wave/10000.),  (UKIRT_Zband_thru/UKIRT_Zband_thru.max()), color='olive',         alpha=alpha, linewidth=linewidth)
-#ax.fill( (UKIRT_Zband_wave/10000.),  (UKIRT_Zband_thru/UKIRT_Zband_thru.max()), color='olive',         alpha=alpha/2)
-ax.plot( (UKIRT_Yband_wave/10000.),  (UKIRT_Yband_thru/UKIRT_Yband_thru.max()), color='darkgoldenrod', alpha=alpha, linewidth=linewidth)
-ax.fill( (UKIRT_Yband_wave/10000.),  (UKIRT_Yband_thru/UKIRT_Yband_thru.max()), color='darkgoldenrod', alpha=alpha/2)
-ax.plot( (UKIRT_Jband_wave/10000.),  (UKIRT_Jband_thru/UKIRT_Jband_thru.max()), color='orange',        alpha=alpha, linewidth=linewidth)
-ax.fill( (UKIRT_Jband_wave/10000.),  (UKIRT_Jband_thru/UKIRT_Jband_thru.max()), color='orange',        alpha=alpha/2)
-ax.plot( (UKIRT_Hband_wave/10000.),  (UKIRT_Hband_thru/UKIRT_Hband_thru.max()), color='gold',          alpha=alpha, linewidth=linewidth)
-ax.fill( (UKIRT_Hband_wave/10000.),  (UKIRT_Hband_thru/UKIRT_Hband_thru.max()), color='gold',          alpha=alpha/2)
-ax.plot( (UKIRT_Kband_wave/10000.),  (UKIRT_Kband_thru/UKIRT_Kband_thru.max()), color='yellow',        alpha=alpha, linewidth=linewidth)
-ax.fill( (UKIRT_Kband_wave/10000.),  (UKIRT_Kband_thru/UKIRT_Kband_thru.max()), color='yellow',        alpha=alpha/2)
+if plot_UKIRT== 'y':
+    ##  UKIRT
+    ax.plot( (UKIRT_Zband_wave/10000.),  (UKIRT_Zband_thru/UKIRT_Zband_thru.max()), color='olive',         alpha=alpha, linewidth=linewidth)
+    ax.fill( (UKIRT_Zband_wave/10000.),  (UKIRT_Zband_thru/UKIRT_Zband_thru.max()), color='olive',         alpha=alpha/2)
+    ax.plot( (UKIRT_Yband_wave/10000.),  (UKIRT_Yband_thru/UKIRT_Yband_thru.max()), color='darkgoldenrod', alpha=alpha, linewidth=linewidth)
+    ax.fill( (UKIRT_Yband_wave/10000.),  (UKIRT_Yband_thru/UKIRT_Yband_thru.max()), color='darkgoldenrod', alpha=alpha/2)
+    ax.plot( (UKIRT_Jband_wave/10000.),  (UKIRT_Jband_thru/UKIRT_Jband_thru.max()), color='orange',        alpha=alpha, linewidth=linewidth)
+    ax.fill( (UKIRT_Jband_wave/10000.),  (UKIRT_Jband_thru/UKIRT_Jband_thru.max()), color='orange',        alpha=alpha/2)
+    ax.plot( (UKIRT_Hband_wave/10000.),  (UKIRT_Hband_thru/UKIRT_Hband_thru.max()), color='gold',          alpha=alpha, linewidth=linewidth)
+    ax.fill( (UKIRT_Hband_wave/10000.),  (UKIRT_Hband_thru/UKIRT_Hband_thru.max()), color='gold',          alpha=alpha/2)
+    ax.plot( (UKIRT_Kband_wave/10000.),  (UKIRT_Kband_thru/UKIRT_Kband_thru.max()), color='yellow',        alpha=alpha, linewidth=linewidth)
+    ax.fill( (UKIRT_Kband_wave/10000.),  (UKIRT_Kband_thru/UKIRT_Kband_thru.max()), color='yellow',        alpha=alpha/2)
+    
+    plt.text(0.870, 1.1, r'Z', color ='olive',        fontsize=fontsize,     weight='bold')
+    plt.text(1.000, 1.1, r'Y', color ='darkgoldenrod', fontsize=fontsize,     weight='bold')
+    plt.text(1.280, 1.1, r'J', color ='orange',        fontsize=fontsize,     weight='bold')
+    plt.text(1.600, 1.1, r'H', color ='gold',          fontsize=fontsize,     weight='bold')
+    #plt.text(2.020, 1.1, r'K', color ='k',            fontsize=fontsize*1.2, weight='bold')
+    plt.text(2.020, 1.1, r'K', color ='yellow',        fontsize=fontsize,     weight='bold')
 
-#plt.text(0.870, 1.1, r'Z', color ='olive',        fontsize=fontsize,     weight='bold')
-plt.text(1.000, 1.1, r'Y', color ='darkgoldenrod', fontsize=fontsize,     weight='bold')
-plt.text(1.280, 1.1, r'J', color ='orange',        fontsize=fontsize,     weight='bold')
-plt.text(1.600, 1.1, r'H', color ='gold',          fontsize=fontsize,     weight='bold')
-#plt.text(2.020, 1.1, r'K', color ='k',            fontsize=fontsize*1.2, weight='bold')
-plt.text(2.020, 1.1, r'K', color ='yellow',        fontsize=fontsize,     weight='bold')
-
+if plot_VISTA == 'y':
+    ##  VISTA
+    ax.plot( (VISTA_Zband_wave/10000.), (VISTA_Zband_thru/VISTA_Zband_thru.max()), color='olive',         alpha=alpha, linewidth=linewidth)
+    ax.fill( (VISTA_Zband_wave/10000.), (VISTA_Zband_thru/VISTA_Zband_thru.max()), color='olive',         alpha=alpha/2)
+    ax.plot( (VISTA_Yband_wave/10000.), (VISTA_Yband_thru/VISTA_Yband_thru.max()), color='darkgoldenrod', alpha=alpha, linewidth=linewidth)
+    ax.fill( (VISTA_Yband_wave/10000.), (VISTA_Yband_thru/VISTA_Yband_thru.max()), color='darkgoldenrod', alpha=alpha/2)
+    ax.plot( (VISTA_Jband_wave/10000.), (VISTA_Jband_thru/VISTA_Jband_thru.max()), color='orange',        alpha=alpha, linewidth=linewidth)
+    ax.fill( (VISTA_Jband_wave/10000.), (VISTA_Jband_thru/VISTA_Jband_thru.max()), color='orange',        alpha=alpha/2)
+    ax.plot( (VISTA_Hband_wave/10000.), (VISTA_Hband_thru/VISTA_Hband_thru.max()), color='gold',          alpha=alpha, linewidth=linewidth)
+    ax.fill( (VISTA_Hband_wave/10000.), (VISTA_Hband_thru/VISTA_Hband_thru.max()), color='gold',          alpha=alpha/2)
+    ax.plot( (VISTA_Ksband_wave/10000.), (VISTA_Ksband_thru/VISTA_Ksband_thru.max()), color='yellow',        alpha=alpha, linewidth=linewidth)
+    ax.fill( (VISTA_Ksband_wave/10000.), (VISTA_Ksband_thru/VISTA_Ksband_thru.max()), color='yellow',        alpha=alpha/2)
+       
 ## 2MASS
-ax.plot( (TwoMASS_Ksband_wave/10000.), (TwoMASS_Ksband_thru/TwoMASS_Ksband_thru.max()), color='khaki', alpha=alpha, linewidth=linewidth, linestyle='-')
-ax.fill( (TwoMASS_Ksband_wave/10000.), (TwoMASS_Ksband_thru/TwoMASS_Ksband_thru.max()), color='khaki', alpha=alpha/2.)
-
-plt.text(2.200, 1.1, r'K$_{s}$', color ='khaki', fontsize=fontsize, style='italic', weight='bold')
-
+#ax.plot( (TwoMASS_Ksband_wave/10000.), (TwoMASS_Ksband_thru/TwoMASS_Ksband_thru.max()), color='khaki', alpha=alpha, linewidth=linewidth, linestyle='-')
+#ax.fill( (TwoMASS_Ksband_wave/10000.), (TwoMASS_Ksband_thru/TwoMASS_Ksband_thru.max()), color='khaki', alpha=alpha/2.)
+#
+#plt.text(2.200, 1.1, r'K$_{s}$', color ='khaki', fontsize=fontsize, style='italic', weight='bold')
 
 ## WISE
-ax.plot( (W1_band_wave),  (W1_band_thru/W1_band_thru.max()), color='peru',      alpha=0.85, linewidth=linewidth)
-ax.fill( (W1_band_wave),  (W1_band_thru/W1_band_thru.max()), color='peru',      alpha=alpha/2)
-ax.plot( (W2_band_wave),  (W2_band_thru/W2_band_thru.max()), color='orangered', alpha=0.85, linewidth=linewidth)
-ax.fill( (W2_band_wave),  (W2_band_thru/W2_band_thru.max()), color='orangered', alpha=alpha/2)
-ax.plot( (W3_band_wave),  (W3_band_thru/W3_band_thru.max()), color='red',       alpha=0.85, linewidth=linewidth)
-ax.fill( (W3_band_wave),  (W3_band_thru/W3_band_thru.max()), color='red',       alpha=alpha/2)
-ax.plot( (W4_band_wave),  (W4_band_thru/W4_band_thru.max()), color='darkred',   alpha=0.85, linewidth=linewidth)
-ax.fill( (W4_band_wave),  (W4_band_thru/W4_band_thru.max()), color='darkred',   alpha=alpha/2)
-plt.text( 3.280, 1.1, r'W1', color ='peru',      fontsize=fontsize, weight='bold')
-plt.text( 4.200, 1.1, r'W2', color ='orangered', fontsize=fontsize, weight='bold')
-plt.text(10.120, 1.1, r'W3', color ='red',       fontsize=fontsize, weight='bold')
-plt.text(19.120, 1.1, r'W4', color ='darkred',   fontsize=fontsize, weight='bold')
-
+if plot_WISE  == 'y': 
+    ax.plot( (W1_band_wave),  (W1_band_thru/W1_band_thru.max()), color='peru',      alpha=0.85, linewidth=linewidth)
+    ax.fill( (W1_band_wave),  (W1_band_thru/W1_band_thru.max()), color='peru',      alpha=alpha/2)
+    ax.plot( (W2_band_wave),  (W2_band_thru/W2_band_thru.max()), color='orangered', alpha=0.85, linewidth=linewidth)
+    ax.fill( (W2_band_wave),  (W2_band_thru/W2_band_thru.max()), color='orangered', alpha=alpha/2)
+    ax.plot( (W3_band_wave),  (W3_band_thru/W3_band_thru.max()), color='red',       alpha=0.85, linewidth=linewidth)
+    ax.fill( (W3_band_wave),  (W3_band_thru/W3_band_thru.max()), color='red',       alpha=alpha/2)
+    ax.plot( (W4_band_wave),  (W4_band_thru/W4_band_thru.max()), color='darkred',   alpha=0.85, linewidth=linewidth)
+    ax.fill( (W4_band_wave),  (W4_band_thru/W4_band_thru.max()), color='darkred',   alpha=alpha/2)
+    
+    plt.text( 3.280, 1.1, r'W1', color ='peru',      fontsize=fontsize, weight='bold')
+    plt.text( 4.200, 1.1, r'W2', color ='orangered', fontsize=fontsize, weight='bold')
+    plt.text(10.120, 1.1, r'W3', color ='red',       fontsize=fontsize, weight='bold')
+    plt.text(19.120, 1.1, r'W4', color ='darkred',   fontsize=fontsize, weight='bold')
+    
 
 ##
 ## L/T spectra
 ##
 L1_flux = L1_flux/ np.nanmax(L1_flux)
 #ax.plot(L1_wave[0::5],  L1_flux[0::5], color='r', alpha=1.0, linewidth=linewidth/2.)
-ax.plot(L1_wave[0::20],  L1_flux[0::20], color='r', alpha=1.0, linewidth=linewidth)
+ax.plot(L1_wave[0::20],  L1_flux[0::20], color='r', alpha=1.0, linewidth=linewidth, linestyle='--')
 
 L3pnt5_flux = L3pnt5_flux/ np.nanmax(L3pnt5_flux)
 ##ax.plot(L3pnt5_wave,  L3pnt5_flux, color='firebrick', alpha=1.0, linewidth=linewidth/10.)
@@ -380,8 +431,10 @@ Banados_weak_flux    = Banados_weak_flux    / Banados_weak_flux.max()
 
 ## Plot a redshifted quasar spectrum
 print()
-redshift_in = float(input("What redshift is the quasar? "))
+#redshift_in = float(input("What redshift is the quasar? "))
+redshift_in = 6.7
 redshift = redshift_in
+
 ax.plot( ((VdB01_wave/10000.)*(1.0+redshift)),  (quasar_template_flux), color='k',alpha=1.0, linewidth=linewidth)
 #ax.plot( ((Banados_wave/10000.)*(1.0+redshift)), (Banados_comp_flux), color='b', alpha=0.85)
 ##ax.plot( ((Glik_wave/10000.0)*(1.0+redshift)),  (Glik_flux), color='dimgray', alpha=1.0, linewidth=linewidth/2)
@@ -390,9 +443,10 @@ ax.plot( ((VdB01_wave/10000.)*(1.0+redshift)),  (quasar_template_flux), color='k
 ##ax.plot( ((Banados_wave/10000.)*(1.0+redshift)), (Banados_weak_flux), color='b', alpha=0.85)
 
 ## Labels
-size=18
-x_placement = 2.40    ## Stopping at W2
-# x_placement = 5.5  ## With W4
+#size=18   ## 18 if between K  and W1; 
+size=22    ## 22 if between W2 and W3. 
+#x_placement = 2.40    ## Stopping at W2
+x_placement = 5.5      ## With W4
 plt.text(x_placement, (ymax*0.72), r'$z$='+str(redshift)+' quasar', color='k',     weight='bold', size=size)
 plt.text(x_placement, (ymax*0.64), r'L1 dwarf',                     color='r',     weight='bold', size=size)
 plt.text(x_placement, (ymax*0.56), r'T4.5 dwarf',                   color='brown', weight='bold', size=size)
