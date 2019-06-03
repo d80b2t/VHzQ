6131b6ad3f3af04f273c105bfaf4c6bbe2ee9f7d
npross <npross@lbl.gov>
2019-06-03 17:29:19 +0100

    big unWISE_redshift_det.py update

--- tables_for_paper/unWISE_redshift_det.py
+++ tables_for_paper/unWISE_redshift_det.py
@@ -73,7 +73,7 @@ print('\hline \hline')
 ## By redshift
 redshift_array = np.array([5.00, 5.70, 6.00, 6.19, 6.50, 6.78, 7.00, 7.50])
 
-snr_cut = 2.0
+snr_cut = 3.0
 
 ## How many of the VHzQs are above a given redshift 
 N_zgt5pnt00 = np.count_nonzero(VHzQs['redshift'] >= redshift_array[0]) 
@@ -130,37 +130,43 @@ N_zgt7pnt50_ALLW4 = np.count_nonzero((VHzQs['redshift'] >= redshift_array[7]) & 
 
 print()
 print()
+print('\hline \hline')
+print(' $z \geq$ & unWISE W1 & unWISE W2 & ALLWISE W3 & ALLWISE W4  \\\ ')
+print('\hline')
 
-print(' 5.00   {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) '.format(N_zgt5pnt00_unW1,  ((N_zgt5pnt00_unW1  / N_zgt5pnt00)*100),
+print(' 5.00   & {:5d} ({:5.1f}\%)   & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%) \\\ '.format(N_zgt5pnt00_unW1,  ((N_zgt5pnt00_unW1  / N_zgt5pnt00)*100),
                                                                                                 N_zgt5pnt00_unW2,  ((N_zgt5pnt00_unW2  / N_zgt5pnt00)*100),
                                                                                                 N_zgt5pnt00_ALLW3, ((N_zgt5pnt00_ALLW3 / N_zgt5pnt00)*100),
                                                                                                 N_zgt5pnt00_ALLW4, ((N_zgt5pnt00_ALLW4 / N_zgt5pnt00)*100)))
-print(' 5.70   {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) '.format(N_zgt5pnt70_unW1,  ((N_zgt5pnt70_unW1  / N_zgt5pnt70)*100),
+print(' 5.70   & {:5d} ({:5.1f}\%)   & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%) \\\ '.format(N_zgt5pnt70_unW1,  ((N_zgt5pnt70_unW1  / N_zgt5pnt70)*100),
                                                                                                 N_zgt5pnt70_unW2,  ((N_zgt5pnt70_unW2  / N_zgt5pnt70)*100),
                                                                                                 N_zgt5pnt70_ALLW3, ((N_zgt5pnt70_ALLW3 / N_zgt5pnt70)*100),
                                                                                                 N_zgt5pnt70_ALLW4, ((N_zgt5pnt70_ALLW4 / N_zgt5pnt70)*100)))
-print(' 6.00   {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) '.format(N_zgt6pnt00_unW1,  ((N_zgt6pnt00_unW1  / N_zgt6pnt00)*100),
+print(' 6.00   & {:5d} ({:5.1f}\%)   & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%) \\\ '.format(N_zgt6pnt00_unW1,  ((N_zgt6pnt00_unW1  / N_zgt6pnt00)*100),
                                                                                                 N_zgt6pnt00_unW2,  ((N_zgt6pnt00_unW2  / N_zgt6pnt00)*100),
                                                                                                 N_zgt6pnt00_ALLW3, ((N_zgt6pnt00_ALLW3 / N_zgt6pnt00)*100),
                                                                                                 N_zgt6pnt00_ALLW4, ((N_zgt6pnt00_ALLW4 / N_zgt6pnt00)*100)))
-print(' 6.19   {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) '.format(N_zgt6pnt19_unW1,  ((N_zgt6pnt19_unW1  / N_zgt6pnt19)*100),
+print(' 6.19   & {:5d} ({:5.1f}\%)   & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%) \\\ '.format(N_zgt6pnt19_unW1,  ((N_zgt6pnt19_unW1  / N_zgt6pnt19)*100),
                                                                                                 N_zgt6pnt19_unW2,  ((N_zgt6pnt19_unW2  / N_zgt6pnt19)*100),
                                                                                                 N_zgt6pnt19_ALLW3, ((N_zgt6pnt19_ALLW3 / N_zgt6pnt19)*100),
                                                                                                 N_zgt6pnt19_ALLW4, ((N_zgt6pnt19_ALLW4 / N_zgt6pnt19)*100)))
-print(' 6.50   {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) '.format(N_zgt6pnt50_unW1,  ((N_zgt6pnt50_unW1  / N_zgt6pnt50)*100),
+print(' 6.50   & {:5d} ({:5.1f}\%)   & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%) \\\ '.format(N_zgt6pnt50_unW1,  ((N_zgt6pnt50_unW1  / N_zgt6pnt50)*100),
                                                                                                 N_zgt6pnt50_unW2,  ((N_zgt6pnt50_unW2  / N_zgt6pnt50)*100),
                                                                                                 N_zgt6pnt50_ALLW3, ((N_zgt6pnt50_ALLW3 / N_zgt6pnt50)*100),
                                                                                                 N_zgt6pnt50_ALLW4, ((N_zgt6pnt50_ALLW4 / N_zgt6pnt50)*100)))
-print(' 6.78   {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) '.format(N_zgt6pnt78_unW1,  ((N_zgt6pnt78_unW1  / N_zgt6pnt78)*100),
+print(' 6.78   & {:5d} ({:5.1f}\%)   & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%) \\\ '.format(N_zgt6pnt78_unW1,  ((N_zgt6pnt78_unW1  / N_zgt6pnt78)*100),
                                                                                                 N_zgt6pnt78_unW2,  ((N_zgt6pnt78_unW2  / N_zgt6pnt78)*100),
                                                                                                 N_zgt6pnt78_ALLW3, ((N_zgt6pnt78_ALLW3 / N_zgt6pnt78)*100),
                                                                                                 N_zgt6pnt78_ALLW4, ((N_zgt6pnt78_ALLW4 / N_zgt6pnt78)*100)))
-print(' 7.00   {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) '.format(N_zgt7pnt00_unW1,  ((N_zgt7pnt00_unW1  / N_zgt7pnt00)*100),
+print(' 7.00   & {:5d} ({:5.1f}\%)   & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%) \\\ '.format(N_zgt7pnt00_unW1,  ((N_zgt7pnt00_unW1  / N_zgt7pnt00)*100),
                                                                                                 N_zgt7pnt00_unW2,  ((N_zgt7pnt00_unW2  / N_zgt7pnt00)*100),
                                                                                                 N_zgt7pnt00_ALLW3, ((N_zgt7pnt00_ALLW3 / N_zgt7pnt00)*100),
                                                                                                 N_zgt7pnt00_ALLW4, ((N_zgt7pnt00_ALLW4 / N_zgt7pnt00)*100)))
-print(' 7.50   {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) {:5d} ({:5.1f}\%) '.format(N_zgt7pnt50_unW1,  ((N_zgt7pnt50_unW1  / N_zgt7pnt50)*100),
+print(' 7.50   & {:5d} ({:5.1f}\%)   & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%)  & {:5d} ({:5.1f}\%) \\\ '.format(N_zgt7pnt50_unW1,  ((N_zgt7pnt50_unW1  / N_zgt7pnt50)*100),
                                                                                                 N_zgt7pnt50_unW2,  ((N_zgt7pnt50_unW2  / N_zgt7pnt50)*100),
                                                                                                 N_zgt7pnt50_ALLW3, ((N_zgt7pnt50_ALLW3 / N_zgt7pnt50)*100),
                                                                                                 N_zgt7pnt50_ALLW4, ((N_zgt7pnt50_ALLW4 / N_zgt7pnt50)*100)))
+
+print('\hline \hline')
+print()
 
