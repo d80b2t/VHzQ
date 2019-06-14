a7722a01afca1e0d1827d0312a71e847255ee506
npross <npross@lbl.gov>
2019-06-14 13:54:56 +0100

    tables_for_paper/output_table_top5pc.tex

--- tables_for_paper/format_for_tables.py
+++ tables_for_paper/format_for_tables.py
@@ -14,7 +14,8 @@ from astropy.table import Table
 ##  V H z Q    data
 ##
 path      = '/cos_pc19a_npr/programs/quasars/highest_z/tables_for_paper/'
-filename  = 'WSA_wWISE.dat'
+#filename  = 'WSA_wWISE.dat'
+filename   = 'VHzQs_ZYJHK_WISE.dat' 
 table     = path+filename
 VHzQ_list = ascii.read(table)
 
@@ -28,16 +29,17 @@ VHzQ_Top10 = VHzQ_list[0:23]
 
 ## Set-up the table header
 header = """\\begin{table}
-\\begin{tabular}{llrrc ccccc cccc}
+\\begin{tabular}{llrrc cccc cccc}
  \hline
  \hline
-  \multirow{2}{*}{Survey} &  \multirow{2}{*}{QsoName} &   R.A. / deg  &   Decl. / deg  &  \multirow{2}{*}{redshift} & \multirow{2}{*}{Z}  &  \multirow{2}{*}{Y}  &  \multirow{2}{*}{J}   &  \multirow{2}{*}{H}  &  \multirow{2}{*}{K}     &  \multicolumn{2}{c}{unWISE}  &  \multicolumn{2}{c}{AllWISE} \\\ 
-                                     &                                            &   (J2000)       &  (J2000)         &                                        &                              &                               &                                &                                &                                  &                    W1       & W2        & W3   & W4 \\\ 
+  \multirow{2}{*}{Survey} &  \multirow{2}{*}{QsoName} &   R.A. / deg  &   Decl. / deg  &  \multirow{2}{*}{redshift}   &  \multirow{2}{*}{Y}  &  \multirow{2}{*}{J}   &  \multirow{2}{*}{H}  &  \multirow{2}{*}{K}     &  \multicolumn{2}{c}{unWISE}  &  \multicolumn{2}{c}{AllWISE} \\\ 
+                          &                           &   (J2000)     &  (J2000)       &                              &                      &                       &                      &                         &          W1       & W2       & W3   & W4 \\\ 
   \hline
   \hline
   \\\\\n"""
 
 print(header)
+
 
 ## Set-up the table footer...
 footer = """    \hline
@@ -49,12 +51,13 @@ footer = """    \hline
      \n"""
 print(footer)
 
-
+##  {0}, {1}, {2}, {3}, {4}, are Survey, QsoName, ra, dec, redshift
+##  
 with open("output_table.tex", "w") as out:
    out.write(header)
    #for row in VHzQ_list:
    for row in VHzQ_Top10:
-       out.write("{0} & {1} & {2:10.5f} & {3:10.5f} & {4:5.2f}   &   ${5:5.2f}\\pm{6:5.3f}$  &  ${7:5.2f}\\pm{8:5.3f}$  &  ${9:5.2f}\\pm{10:5.3f}$  &  ${11:5.2f}\\pm{12:5.3f}$   & ${13:5.2f}\\pm{14:5.3f}$    &   ${15:5.3f}\\pm{16:5.3f}$   &  ${18:5.2f}\\pm{19:5.3f}$   &   ${21:5.2f}\\pm{22:5.3f}$   &   ${24:5.2f}\\pm{25:5.3f}$   \\\\\n".format(*row))
+       out.write("{0} & {1} & {2:10.5f} & {3:10.5f} & {4:5.2f}   &   ${7:5.2f}\\pm{8:5.3f}$  &  ${9:5.2f}\\pm{10:5.3f}$  &  ${11:5.2f}\\pm{12:5.3f}$   & ${13:5.2f}\\pm{14:5.3f}$    &   ${15:5.3f}\\pm{16:5.3f}$   &  ${18:5.2f}\\pm{19:5.3f}$   &   ${21:5.2f}\\pm{22:5.3f}$   &   ${24:5.2f}\\pm{25:5.3f}$   \\\\\n".format(*row))
    out.write(footer)
 
    
