501d5d9b6ec43ddaf848ab6147435e5df515dff7
npross <npross@lbl.gov>
2019-06-14 20:59:14 +0100

    color_color/W1W2_plots/W1W2W3_colors.py update

--- color_color/W1W2_plots/W1W2W3_colors.py
+++ color_color/W1W2_plots/W1W2W3_colors.py
@@ -32,12 +32,24 @@ print()
 ### GOOD 
 bluest_W1andW3  = W3_goodSNR[np.where((W3_goodSNR['unW1mag']-W3_goodSNR['w3mpro'])  ==
                                       (W3_goodSNR['unW1mag']-W3_goodSNR['w3mpro']).min())]
+reddest_W1andW3 = W3_goodSNR[np.where((W3_goodSNR['unW1mag']-W3_goodSNR['w3mpro'])  ==
+                                      (W3_goodSNR['unW1mag']-W3_goodSNR['w3mpro']).max())]
 
-reddest_W1andW3 = W3_goodSNR[np.where( (W3_goodSNR['unW2mag']-W3_goodSNR['w3mpro'])  ==
-                                        (W3_goodSNR['unW2mag']-W3_goodSNR['w3mpro']).max())]
+
+bluest_W2andW3 = W3_goodSNR[np.where((W3_goodSNR['unW2mag']-W3_goodSNR['w3mpro'])  ==
+                                     (W3_goodSNR['unW2mag']-W3_goodSNR['w3mpro']).min())]
+
+reddest_W2andW3 = W3_goodSNR[np.where((W3_goodSNR['unW2mag']-W3_goodSNR['w3mpro'])  ==
+                                      (W3_goodSNR['unW2mag']-W3_goodSNR['w3mpro']).max())]
 
 print()
 print('Number of objects with good (>3.0) W3 SNR : ', len(W3_goodSNR))
-print('    of which ',  bluest_W1andW2['na'][0],  bluest_W1andW2['desig'][0], '    is the bluest with (W1-W2) =  ', bluest_W1andW2['unW1mag'][0]-bluest_W1andW2['unW2mag'][0] )
-print('    of which ', reddest_W1andW2['na'][0], reddest_W1andW2['desig'][0], ' is the reddest with (W1-W2) =  ',   reddest_W1andW2['unW1mag'][0]-reddest_W1andW2['unW2mag'][0] )
+print('    of which ',  bluest_W1andW3['na'][0],  bluest_W1andW3['desig'][0], '    is the bluest with (W1-W3) =  ', bluest_W1andW3['unW1mag'][0]  - bluest_W1andW3['w3mpro'][0] )
+print('    of which ', reddest_W1andW3['na'][0], reddest_W1andW3['desig'][0], ' is the reddest with (W1-W3) =  ',   reddest_W1andW3['unW1mag'][0] - reddest_W1andW3['w3mpro'][0] )
+print('    of which ',  bluest_W2andW3['na'][0],  bluest_W2andW3['desig'][0], '    is the bluest with (W2-W3) =  ', bluest_W2andW3['unW2mag'][0]  - bluest_W1andW3['w3mpro'][0] )
+print('    of which ', reddest_W2andW3['na'][0], reddest_W2andW3['desig'][0], ' is the reddest with (W2-W3) =  ',   reddest_W2andW3['unW2mag'][0] - reddest_W1andW3['w3mpro'][0] )
+
 print()
+
+## bluest (W1-W3) and (W2-W3)::    SDSS J0100+2802 15.0542635 28.040532     6.33
+## reddes (W1-W3) and (W2-W3):: PSO J075.9356-07.5061 
