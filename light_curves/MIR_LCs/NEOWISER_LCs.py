3c58e8e84de26c87c1d38a441544764f4582a4ad
npross <npross@lbl.gov>
2019-06-14 16:29:14 +0100

    ight_curves/MIR_LCs/NEOWISER_LCs.py

--- light_curves/MIR_LCs/NEOWISER_LCs.py
+++ light_curves/MIR_LCs/NEOWISER_LCs.py
@@ -26,7 +26,6 @@ file  = 'NEOWISER-R_SingleExposure_L1bs_20190429_averaged.tbl'
 avers = ascii.read(path+file) 
 
 
-
 out_pdf = 'NEOWISER_LCs_VHzQs_temp.pdf'
 pdf_pages = PdfPages(out_pdf)
 ## http://wise2.ipac.caltech.edu/docs/release/allwise/expsup/sec1_2.html#phases
@@ -56,100 +55,101 @@ cmap = plt.cm.inferno_r
 
 ls       = 'solid'
 lw       = 2.0
-ms       = 60.
+ms       = 30.
 ms_large = ms*5.
-ms_full  = ms*10
+ms_full  = ms*5.
 fontsize = 24
 alpha    = 1.00
 plot_num = 321
 totes    = 0
 
 for x in range(upto_this):
-#for x in range(10):
+    #for x in range(10):
     x=x+1
-    data_one = data[np.where(data['cntr_01'] == x)]
-
-    if len(data_one) < 1:
-          #print(x, 'No of epochs: 0')
-          
-    if len(data_one) > 0:  
-#        print(x, data_one['ra'][0], data_one['dec'][0], len(data_one))
-        #print(x, 'No of epochs:', len(data_one))
-        totes = totes + len(data_one)
-        ra = data_one['ra'][0]
-        dec = data_one['dec'][0]
-
-        ## Setting up a 3 panel, "triptych" style plot
-        fig, (ax1, ax2, ax3) = plt.subplots(1,3, figsize=(21.5, 8.5), gridspec_kw = {'width_ratios':[3, 1, 1]}) # inches
-        #ax1 = plt.subplot(131, figsize=(12.0, 8.5) )
-        #ax2 = plt.subplot(132, figsize=(5.0, 8.5))
-        #ax3 = plt.subplot(133, figsize=(5.0, 8.5), sharey=ax2)
-
-        left   = 0.06   # the left side of the subplots of the figure
-        right  = 0.98   # the right side of the subplots of the figure
-        bottom = 0.10   # the bottom of the subplots of the figure
-        top    = 0.94   # the top of the subplots of the figure
-        wspace = 0.26   # the amount of width reserved for blank space between subplots
-        hspace = 0.06   # the amount of height reserved for white space between subplots
-        plt.subplots_adjust(left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
-
-        ## W1/2 Light-curves, y-axis reverse
-
-        ## ALLWISE SINGLE MJD POINT
-        ax1.scatter(ALLWISE_MJD_mid,  data_one['w1mpro_allwise'][0], color='k', alpha=alpha, s=ms_large*1.8)
-        ax1.scatter(ALLWISE_MJD_mid,  data_one['w1mpro_allwise'][0], color='r', alpha=alpha, s=ms_large)
-        ax1.scatter(ALLWISE_MJD_mid,  data_one['w2mpro_allwise'][0], color='k', alpha=alpha, s=ms_large*1.8)
-        ax1.scatter(ALLWISE_MJD_mid,  data_one['w2mpro_allwise'][0], color='c', alpha=alpha, s=ms_large)
-
-        ## NEOWISE-R data; single-epich L1bs
-        ax1.scatter(data_one['mjd'], data_one['w1mpro'], color='k', alpha=alpha, s=ms*1.8)
-        ax1.scatter(data_one['mjd'], data_one['w1mpro'], color='r', alpha=alpha, s=ms)
-        ax1.scatter(data_one['mjd'], data_one['w2mpro'], color='k', alpha=alpha, s=ms*1.8)
-        ax1.scatter(data_one['mjd'], data_one['w2mpro'], color='c', alpha=alpha, s=ms)
-
-        for y in avers['cntr_01']:
-            print('x, y, avers['cntr_01'][y]',   x,   y,   avers['cntr_01'] )
-#        if (avers['cntr_01'] == x):
-#            ax1.scatter(avers['mean_mjd'], avers['w1mpro_wgt'], yerr=avers['w1mpro_err'], color='k', alpha=alpha, s=ms_full*1.8)
-#            ax1.scatter(avers['mean_mjd'], avers['w1mpro_wgt'], yerr=avers['w1mpro_err'], color='r', alpha=alpha, s=ms_full)
- #           ax1.scatter(avers['mean_mjd'], avers['w2mpro_wgt'], yerr=avers['w2mpro_err'], color='k',alpha=alpha,  s=ms_full*1.8)
-  #          ax1.scatter(avers['mean_mjd'], avers['w2mpro_wgt'], yerr=avers['w2mpro_err'], color='c', alpha=alpha, s=ms_full)
-
-
-        ## AllWISE 4-Band Cryo Survey Phase started MJD 55203
-        ax1.set_xlim((55000,58140))
-        ## Just NEOWISE-R
-        #ax1.set_xlim((56500,58140))
-        ax1.set_ylim(ax1.get_ylim()[::-1])
-        ax1.set_xlabel('MJD',                   fontsize=fontsize)
-        ax1.set_ylabel('WISE  W1/2  magnitude', fontsize=fontsize)
-        ax1.tick_params('x', direction='in', which='both', bottom='True', top='True', left='True', right='True', labelsize=fontsize)
-        ax1.tick_params('y', direction='in', which='both', bottom='True', top='True', left='True', right='True', labelsize=fontsize)
-
-        ## W1 vs. color to look for color-changes...
-        ax2.scatter(data_one['w1mpro'], (data_one['w1mpro']-data_one['w2mpro']),color='k', alpha=alpha, s=ms*1.8)
-        ax2.scatter(data_one['w1mpro'], (data_one['w1mpro']-data_one['w2mpro']),color='r', alpha=alpha, s=ms)
-        ax2.set_xlabel('W1 magnitude',                   fontsize=fontsize)
-        ax2.set_ylabel('W1 - W2', fontsize=fontsize)
-        ax2.tick_params('x', direction='in', which='both', bottom='True', top='True', left='True', right='True', labelsize=fontsize)
-        ax2.tick_params('y', direction='in', which='both', bottom='True', top='True', left='True', right='True', labelsize=fontsize)
-        ax2.set_title('(R.A., Decl.) = {} {} {}'.format(ra,',',dec), fontsize=fontsize)
-
-        ## W2 vs. color to look for color-changes...
-        ax3.scatter(data_one['w2mpro'], (data_one['w1mpro']-data_one['w2mpro']),color='k', alpha=alpha, s=ms*1.8)
-        ax3.scatter(data_one['w2mpro'], (data_one['w1mpro']-data_one['w2mpro']),color='c', alpha=alpha, s=ms)
-        ax3.set_xlabel('W2 magnitude',                   fontsize=fontsize)
-#        ax3.set_ylabel('W1 - W2', fontsize=fontsize)
-        ax3.tick_params('x', direction='in', which='both', bottom='True', top='True', left='True', right='True',  labelsize=fontsize)
-        ax3.tick_params('y', direction='in', which='both', bottom='True', top='True', left='True', right='False', labelsize=fontsize)
-
-        ax2.get_shared_x_axes().join(ax2, ax3)
-        ax3.set_yticklabels([])
-        #plt.subplots_adjust(wspace=.0)
-        
-        ## Done with the page
-        pdf_pages.savefig(fig)
+    data_one =  data[np.where(data['cntr_01'] == x)]
+    data_two = avers[np.where(avers['cntr_01'] == x)]
     
+    if len(data_one) < 1:
+        print(x, 'No of epochs: 0')
+
+    if len(data_one) > 0:
+        # print(x, data_one['ra'][0], data_one['dec'][0], len(data_one))
+        print(x, 'No of epochs:', len(data_one))
+        
+        if len(data_two) > 0:
+            totes = totes + len(data_one)
+            ra  = data_one['ra'][0]
+            dec = data_one['dec'][0]
+    
+            ## Setting up a 3 panel, "triptych" style plot
+            fig, (ax1, ax2, ax3) = plt.subplots(1,3, figsize=(21.5, 8.5), gridspec_kw = {'width_ratios':[3, 1, 1]}) # inches
+            #ax1 = plt.subplot(131, figsize=(12.0, 8.5) )
+            #ax2 = plt.subplot(132, figsize=(5.0, 8.5))
+            #ax3 = plt.subplot(133, figsize=(5.0, 8.5), sharey=ax2)
+    
+            left   = 0.06   # the left side of the subplots of the figure
+            right  = 0.98   # the right side of the subplots of the figure
+            bottom = 0.10   # the bottom of the subplots of the figure
+            top    = 0.94   # the top of the subplots of the figure
+            wspace = 0.26   # the amount of width reserved for blank space between subplots
+            hspace = 0.06   # the amount of height reserved for white space between subplots
+            plt.subplots_adjust(left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
+        
+            ## W1/2 Light-curves, y-axis reverse
+            ## ALLWISE SINGLE MJD POINT
+            ax1.scatter(ALLWISE_MJD_mid,  data_one['w1mpro_allwise'][0], color='k', alpha=alpha, s=ms_large*1.8)
+            ax1.scatter(ALLWISE_MJD_mid,  data_one['w1mpro_allwise'][0], color='r', alpha=alpha, s=ms_large)
+            ax1.scatter(ALLWISE_MJD_mid,  data_one['w2mpro_allwise'][0], color='k', alpha=alpha, s=ms_large*1.8)
+            ax1.scatter(ALLWISE_MJD_mid,  data_one['w2mpro_allwise'][0], color='c', alpha=alpha, s=ms_large)
+    
+            ## NEOWISE-R data; single-epich L1bs
+            ax1.scatter(data_one['mjd'], data_one['w1mpro'], color='k', alpha=alpha, s=ms*1.8)
+            ax1.scatter(data_one['mjd'], data_one['w1mpro'], color='r', alpha=alpha, s=ms)
+            ax1.scatter(data_one['mjd'], data_one['w2mpro'], color='k', alpha=alpha, s=ms*1.8)
+            ax1.scatter(data_one['mjd'], data_one['w2mpro'], color='c', alpha=alpha, s=ms)
+
+            ## plotting the NEOWISE-R L1b averages...
+            ax1.scatter(data_two['mean_mjd'], data_two['w1mpro_wgt'], color='k', alpha=alpha, s=ms_full*1.8)
+            ax1.scatter(data_two['mean_mjd'], data_two['w1mpro_wgt'], color='r', alpha=alpha, s=ms_full)
+            ax1.scatter(data_two['mean_mjd'], data_two['w2mpro_wgt'], color='k',alpha=alpha,  s=ms_full*1.8)
+            ax1.scatter(data_two['mean_mjd'], data_two['w2mpro_wgt'], color='c', alpha=alpha, s=ms_full)
+        
+            ## AllWISE 4-Band Cryo Survey Phase started MJD 55203
+            ax1.set_xlim((55000,58140))
+            ## Just NEOWISE-R
+            #ax1.set_xlim((56500,58140))
+            ax1.set_ylim(ax1.get_ylim()[::-1])
+            ax1.set_xlabel('MJD',                   fontsize=fontsize)
+            ax1.set_ylabel('WISE  W1/2  magnitude', fontsize=fontsize)
+            ax1.tick_params('x', direction='in', which='both', bottom='True', top='True', left='True', right='True', labelsize=fontsize)
+            ax1.tick_params('y', direction='in', which='both', bottom='True', top='True', left='True', right='True', labelsize=fontsize)
+    
+            ## W1 vs. color to look for color-changes...
+            ax2.scatter(data_one['w1mpro'], (data_one['w1mpro']-data_one['w2mpro']),color='k', alpha=alpha, s=ms*1.8)
+            ax2.scatter(data_one['w1mpro'], (data_one['w1mpro']-data_one['w2mpro']),color='r', alpha=alpha, s=ms)
+            ax2.set_xlabel('W1 magnitude',                   fontsize=fontsize)
+            ax2.set_ylabel('W1 - W2', fontsize=fontsize)
+            ax2.tick_params('x', direction='in', which='both', bottom='True', top='True', left='True', right='True', labelsize=fontsize)
+            ax2.tick_params('y', direction='in', which='both', bottom='True', top='True', left='True', right='True', labelsize=fontsize)
+            ax2.set_title('(R.A., Decl.) = {} {} {}'.format(ra,',',dec), fontsize=fontsize)
+    
+            ## W2 vs. color to look for color-changes...
+            ax3.scatter(data_one['w2mpro'], (data_one['w1mpro']-data_one['w2mpro']),color='k', alpha=alpha, s=ms*1.8)
+            ax3.scatter(data_one['w2mpro'], (data_one['w1mpro']-data_one['w2mpro']),color='c', alpha=alpha, s=ms)
+            ax3.set_xlabel('W2 magnitude',                   fontsize=fontsize)
+            #ax3.set_ylabel('W1 - W2', fontsize=fontsize)
+            ax3.tick_params('x', direction='in', which='both', bottom='True', top='True', left='True', right='True',  labelsize=fontsize)
+            ax3.tick_params('y', direction='in', which='both', bottom='True', top='True', left='True', right='False', labelsize=fontsize)
+    
+            ax2.get_shared_x_axes().join(ax2, ax3)
+            ax3.set_yticklabels([])
+            #plt.subplots_adjust(wspace=.0)
+            
+            ## Done with the page
+            pdf_pages.savefig(fig)
+##
+## Come out both if loops..
+##            
 ## Write the PDF document to the disk
 pdf_pages.close()
 
