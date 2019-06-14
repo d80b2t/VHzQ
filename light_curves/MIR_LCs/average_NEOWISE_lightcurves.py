8ef7266f2ca43dfe4e71cf57f8adf3863a1095d3
Nick Cross <njc@roe.ac.uk>
2019-06-14 16:21:22 +0100

    Added in single measurements. Improved code, added more comments

--- light_curves/MIR_LCs/average_NEOWISE_lightcurves.py
+++ light_curves/MIR_LCs/average_NEOWISE_lightcurves.py
@@ -48,42 +48,57 @@ def getAveragePhot(data,mjdRangeMask,band):
 
     mpro=data['w%smpro' % band][mjdRangeMask]
     sigma_mpro=data['w%ssigmpro' % band][mjdRangeMask]
-    weights = numpy.where(sigma_mpro>0.,1./(sigma_mpro*sigma_mpro),0.)
-    sumweight = numpy.sum(weights)
-    if sumweight>0.:
-        mpro_err = math.sqrt(1./sumweight)
-        mpro_weight = numpy.sum(mpro*weights)/sumweight   
+    if mpro.size>=2:
+        weights = numpy.where(sigma_mpro>0.,1./(sigma_mpro*sigma_mpro),0.)
+        sumweight = numpy.sum(weights)
+        if sumweight>0.:
+            mpro_err = math.sqrt(1./sumweight)
+            mpro_weight = numpy.sum(mpro*weights)/sumweight   
+        else:
+            mpro_err=default_value
+            mpro_weight = default_value
     else:
-        mpro_err=default_value
-        mpro_weight = default_value
+        mpro_weight=mpro[0]
+        mpro_err=sigma_mpro[0]
     return mpro_weight,mpro_err
 
+#--------------------------------------------------------------------------------------------
+# Starts here
+# Read in data/light_curves/NEOWISE-R/NEOWISER-R_SingleExposure_L1bs_20190429.tbl or similar file
+
+
 inputTable = sys.argv[1]
+
+# Data is astropy Table in IPAC format....
 
 data= Table.read(inputTable, format='ipac')
 # Useful columns: cntr_01,ra,dec,w1mpro,w1sigmpro,w1snr,w2mpro,w2sigmpro,w2snr,cc_flags,mjd
 
-
+# Select unique sourceIDs 
 sourceIDs=sorted(list(set(data['cntr_01'])))
 outputDataList=[]
 for srcID in sourceIDs:
+    # For each source select data
     srcDataMask=numpy.nonzero(data['cntr_01']==srcID)
     ra,dec=data['ra_01'][srcDataMask][0],data['dec_01'][srcDataMask][0]
+    # Get mjd data for source.
     mjdData= sorted(data['mjd'][srcDataMask])
-    # Group mjd values.
+    # Group mjd values into 6 monthly ranges
     mjdRanges = getMjdRanges(mjdData) # min,mean,max
     # Now select data for each range and average:
     for mjdMin,mjdMean,mjdMax,nMeas in mjdRanges:
-        if nMeas>=2:
-            mjdRangeMask=numpy.nonzero(numpy.logical_and(
-                data['mjd'][srcDataMask]>=mjdMin,data['mjd'][srcDataMask]<=mjdMax))
-            w1mpro_weight,w1mpro_err=getAveragePhot(data,mjdRangeMask,1)
-            w2mpro_weight,w2mpro_err=getAveragePhot(data,mjdRangeMask,2)
-            outputDataList+=[(srcID,ra,dec,mjdMean,w1mpro_weight,w1mpro_err,w2mpro_weight,w2mpro_err,nMeas)]
-
+        mjdRangeMask=numpy.nonzero(numpy.logical_and(
+            data['mjd'][srcDataMask]>=mjdMin,data['mjd'][srcDataMask]<=mjdMax))
+        
+        w1mpro_weight,w1mpro_err=getAveragePhot(data,mjdRangeMask,1)
+        w2mpro_weight,w2mpro_err=getAveragePhot(data,mjdRangeMask,2)
+            
+        outputDataList+=[(srcID,ra,dec,mjdMean,w1mpro_weight,w1mpro_err,w2mpro_weight,w2mpro_err,nMeas)]
 # Output data to table.
-
+print("Complete averaging..")
 out_table = Table(rows=outputDataList, names=('cntr_01', 'ra_01', 'dec_01','mean_mjd','w1mpro_wgt','w1mpro_err',
     'w2mpro_wgt','w2mpro_err','nMeas'), dtype=('i4', 'f8', 'f8','f8','f8','f8','f8','f8','i4'))
 
-out_table.write(inputTable.replace('.tbl','_averaged.tbl'),format='ipac',overwrite=True)
+outputTable=inputTable.replace('.tbl','_averaged.tbl')
+out_table.write(outputTable,format='ipac',overwrite=True)
+print("Written to %s" % outputTable)
