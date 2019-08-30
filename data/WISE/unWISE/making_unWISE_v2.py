aada81c0a2df950ac54ed2cb1f98029e3ca3d016
npross <npross@lbl.gov>
2019-08-30 09:44:51 +0100

    VHzQs tidying up data/WISE

--- data/WISE/unWISE/making_unWISE_v2.py
+++ data/WISE/unWISE/making_unWISE_v2.py
@@ -1,15 +1,19 @@
 '''
-This wee script combines, or simply makes, the unWISE files for the VHzQs.
+This wee script combines, or simply makes, the unWISE files for the
+VHzQs.
 
-v1.0::
-Starting with 'just' the original 424 VHzQs and then added
-the "Halloween" 2018 Oct objects from arXiv:1810.11924-27v1.
-Tidied up version of the Jupyter notebook making_unWISE_W1s.ipynb
+v1.0:: Starting with 'just' the original 424 VHzQs and then added the
+"Halloween" 2018 Oct objects from arXiv:1810.11924-27v1.  Tidied up
+version of the Jupyter notebook making_unWISE_W1s.ipynb
 
-v2.0::
-Takes now *both* the W1 and W2 unWISE input files (e.g. from
-Aaron Meisner and the official DR1 of the
+v1.1:: Takes now *both* the W1 and W2 unWISE input files e.g. from
+Aaron Meisner and the official unWISE Catalog DOI:
+10.3847/1538-4365/aafbea http://catalog.unwise.me/
 
+v2.0:: Takes in the W1 and W2 unWISE input files,
+high_z_qsos_24Aug19-w[1/2].fits from Aaron Meisner, and uses them to
+produce the v2 version of the VHzQ catalogue.  Changed "unWISE"
+variable to "unWISE".
 ''' 
 
 import numpy as np
@@ -18,34 +22,35 @@ from astropy.io import fits
 
 ## http://docs.astropy.org/en/stable/io/fits/index.html
 ## Reading in the unWISE W1 and W2 files::
-december_w1_hdu = fits.open("high_z_qsos_03Dec18-w1.fits")  
-december_w2_hdu = fits.open("high_z_qsos_03Dec18-w2.fits")  
+unWISE_w1_hdu = fits.open("high_z_qsos_24Aug19-w1.fits")  
+unWISE_w2_hdu = fits.open("high_z_qsos_24Aug19-w2.fits")  
 
-print('december_w1_hdu', december_w1_hdu)
+print('unWISE_w1_hdu', unWISE_w1_hdu)
+print('unWISE_w2_hdu', unWISE_w2_hdu)
 
 ## Assuming (correctly) the first extension is a table
-december_w1 = december_w1_hdu[1].data
-december_w2 = december_w2_hdu[1].data
+unWISE_w1 = unWISE_w1_hdu[1].data
+unWISE_w2 = unWISE_w2_hdu[1].data
 
 ## Can look at the columns of said files.
 ## Should be very similiar (!!), even identical to that
 ## of the offical unWISE DR1, Meisner & Schlafly (2019)
-december_w1.columns
+unWISE_w1.columns
 
 
-print("type(december_w1['RA']), len(december_w1['RA'])", type(december_w1['RA']), len(december_w1['RA']))
-print("type(december_w2['RA']), len(december_w2['RA'])", type(december_w2['RA']), len(december_w2['RA']))
+print("type(unWISE_w1['RA']), len(unWISE_w1['RA'])", type(unWISE_w1['RA']), len(unWISE_w1['RA']))
+print("type(unWISE_w2['RA']), len(unWISE_w2['RA'])", type(unWISE_w2['RA']), len(unWISE_w2['RA']))
 print()
 
 ## Setting up the variables for flux -> magnitude (and flux error to mag error) conversions
-unWISE_W1_RA    = december_w1['RA']
-unWISE_W2_RA    = december_w2['RA']
-unWISE_W1_DEC   = december_w1['DEC']
-unWISE_W2_DEC   = december_w2['DEC']
-unWISE_W1_FLUX  = december_w1['FLUX']
-unWISE_W2_FLUX  = december_w2['FLUX']
-unWISE_W1_DFLUX = december_w1['DFLUX']
-unWISE_W2_DFLUX = december_w2['DFLUX']
+unWISE_W1_RA    = unWISE_w1['RA']
+unWISE_W2_RA    = unWISE_w2['RA']
+unWISE_W1_DEC   = unWISE_w1['DEC']
+unWISE_W2_DEC   = unWISE_w2['DEC']
+unWISE_W1_FLUX  = unWISE_w1['FLUX']
+unWISE_W2_FLUX  = unWISE_w2['FLUX']
+unWISE_W1_DFLUX = unWISE_w1['DFLUX']
+unWISE_W2_DFLUX = unWISE_w2['DFLUX']
 
 
 ## From http://astrometry.net/svn/tags/sequels-fp-1/wisecheck.py
